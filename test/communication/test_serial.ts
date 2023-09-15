7ec20fe3545bd3621e3b2751c966ed3c8d0f57bd
Carlos Rojas <carlosj.rojascastillo@gmail.com>
2023-09-15 20:37:22 +0200

    add basic test

---
+++ test/communication/test_serial.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('My Hello World Suite', () => {
+    it('should return true', () => {
+        const result = true;
+        expect(result).to.equal(true);
+    });
+});
