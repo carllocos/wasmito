b717d5ca7127ccc00a06fa75c058e1a77d969ec8
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-12 22:00:16 +0200

    add initial device config parser

---
+++ src/device/device_config.ts
@@ -0,0 +1,102 @@
+import { createLogger } from '../logger/logger';
+import * as fs from 'fs';
+
+enum DeviceMode {
+  Emulated,
+  MCU,
+  Proxied,
+  Mirror,
+}
+
+export interface DeviceConfig {
+  name: string;
+  id: string;
+  mode: DeviceMode;
+  port: string;
+  program: string;
+}
+
+export interface DevicesConfig {
+  devices: string[];
+}
+
+function isValidDeviceConfig(value: any): string[] {
+  const errors: string[] = [];
+
+  if (typeof value !== 'object') {
+    errors.push('Device config is not valid json');
+  } else {
+    if (typeof value.name !== 'string') {
+      errors.push('Property "name" should be a string');
+    }
+
+    if (typeof value.id !== 'string') {
+      errors.push('Property "id" should be a string');
+    }
+
+    if (typeof value.port !== 'string') {
+      errors.push('Property "port" should be a string');
+    }
+
+    if (typeof value.program !== 'string') {
+      errors.push('Property "program" should be a string');
+    }
+
+    if (!Object.values(DeviceMode).includes(value.mode)) {
+      errors.push(
+        `Property "mode" is not a valid DeviceMode (choice ${Object.values(
+          DeviceMode,
+        ).toString()}`,
+      );
+    }
+  }
+
+  return errors;
+}
+
+const Logger = createLogger('DeviceConfiguration');
+
+function isValidDevicesConfig(value: any): string[] {
+  const errors: string[] = [];
+  if (typeof value !== 'object') {
+    errors.push('Devices config is not valid json');
+  } else {
+    if (!Array.isArray(value.devices)) {
+      errors.push('Property "devices" should be an array of DeviceConfig');
+    } else {
+      for (const device of value.devices) {
+        const deviceErrors = isValidDeviceConfig(device);
+        if (deviceErrors.length > 0) {
+          return deviceErrors;
+        }
+      }
+    }
+  }
+  return errors;
+}
+
+export function parseDeviceConfig(path: any): DevicesConfig | undefined {
+  let fileContent: any;
+  try {
+    fileContent = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    Logger.error(`Could not read from json file ${path}:`, err);
+    return undefined;
+  }
+
+  let jsonData: any;
+  try {
+    // Parse the JSON data into a JavaScript object
+    jsonData = JSON.parse(fileContent);
+  } catch (err) {
+    Logger.error(`File content could is invalid json: `, err);
+    return undefined;
+  }
+
+  const errors = isValidDevicesConfig(jsonData);
+  if (errors.length > 0) {
+    Logger.error(`Invalid configuration: `, errors.join(' '));
+    return undefined;
+  }
+  return jsonData;
+}
