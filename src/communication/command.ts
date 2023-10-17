bf9da2c041c7c9a8f84032800a174f2cb0471ee1
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-17 14:53:01 +0200

    on successful parse remove listener + add timeout

--- src/communication/command.ts
+++ src/communication/command.ts
@@ -1,38 +1,90 @@
+import { getGlobalLogger } from '../logger/logger';
 import { type APIRequest } from '../warduino/api/request_interface';
 import { type Channel } from './channel_interface';
-
-type FutureResolver<T> = (value: T | PromiseLike<T>) => void;
-type FutureRejecotor = (reason?: any) => void;
 
 export class Command<T> {
   private readonly connection: Channel;
   private readonly request: APIRequest<T>;
-  private requestResolver: FutureResolver<T> | undefined;
-  private requestRejector: FutureRejecotor | undefined;
+  private requestResolver: ((v: T) => void) | undefined;
+  private requestRejector: ((v?: any) => void) | undefined;
+  private resolved: boolean;
+  private rejected: boolean;
+  private readonly onDataListener: (data: string) => void;
+  private readonly timeout: number | undefined;
 
-  constructor(connection: Channel, request: APIRequest<T>) {
+  constructor(connection: Channel, request: APIRequest<T>, timeout?: number) {
     this.connection = connection;
     this.request = request;
+    this.resolved = false;
+    this.rejected = false;
+    this.onDataListener = (data: string) => {
+      this.update(data);
+      if (this.resolved || this.rejected) {
+        this.connection.removeOnData(this.onDataListener);
+      }
+    };
+    this.timeout = timeout;
   }
 
   update(data: string): void {
     try {
-      if (this.requestResolver != null) {
-        this.requestResolver(this.request.parse(data));
+      if (this.requestResolver !== undefined) {
+        const parsed = this.request.parse(data);
+        this.requestResolver(parsed);
       }
-    } catch (APIRequestInvalidParse) {
-    } finally {
-      this.connection.removeOnData(this.update);
+    } catch (APIRequestInvalidParse) {}
+  }
+
+  timedout(): void {
+    if (this.requestRejector !== undefined && this.timeout !== undefined) {
+      const errMsg = `Command timedout after ${this.timeout} ms while waiting for reply`;
+      getGlobalLogger().error(errMsg);
+      this.requestRejector(new CommandError(errMsg));
     }
   }
 
   async execute(): Promise<T> {
-    return await new Promise((resolve, reject) => {
-      this.requestResolver = resolve;
-      this.requestRejector = reject;
+    return await new Promise<T>((resolve, reject) => {
+      this.requestResolver = (v: T) => {
+        if (!this.resolved && !this.rejected) {
+          this.resolved = true;
+          resolve(v);
+        }
+      };
+      this.requestRejector = (v?: any) => {
+        if (!this.resolved && !this.rejected) {
+          this.rejected = true;
+          reject(v);
+        }
+      };
       const d = this.request.getData() + '\n';
-      this.connection.addOnData(this.update);
-      void this.connection.send(d); // void to cancel terun
+      this.connection.addOnData(this.onDataListener);
+      this.connection
+        .send(d)
+        .then((successful) => {
+          if (successful) {
+            if (this.timeout !== undefined) {
+              setTimeout(() => {
+                this.timedout();
+              }, this.timeout);
+            }
+          } else {
+            this.requestRejector?.(
+              new CommandError('could not send content to channel'),
+            );
+          }
+        })
+        .catch((v) => {
+          this.requestRejector?.(v);
+        });
     });
+  }
+}
+
+export class CommandError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'CommandError';
+    Error.captureStackTrace(this, CommandError);
   }
 }
