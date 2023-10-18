6e62c2397daa964e55f296722ebf8804254fb459
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-18 17:05:07 +0200

    add function to get instruction from str

--- src/warduino/api/request_interface.ts
+++ src/warduino/api/request_interface.ts
@@ -14,3 +14,24 @@ export enum Instruction {
   StartRecording = '50',
   StopRecording = '51',
 }
+
+export function getInstructionFromString(str: string): Instruction | undefined {
+  switch (str) {
+    case '01':
+      return Instruction.Run;
+    case '02':
+      return Instruction.Halt;
+    case '03':
+      return Instruction.Pause;
+    case '04':
+      return Instruction.Step;
+    case '09':
+      return Instruction.Inspect;
+    case '50':
+      return Instruction.AroundFunction;
+    case '51':
+      return Instruction.StopRecording;
+    default:
+      return undefined;
+  }
+}
