63b0f52e4229f55565a9edd905d0554d018c5755
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-19 23:43:10 +0200

    add monitor addr request

---
+++ src/warduino/api/monitor_request.ts
@@ -0,0 +1,144 @@
+import { getGlobalLogger } from '../../logger/logger';
+import { encodeLEB128ToHex } from '../../util/encoder';
+import {
+  type APIRequest,
+  Instruction,
+  APIRequestInvalidParse,
+  getInstructionFromString,
+} from './request_interface';
+
+import { type InstrumentAction } from '../../instrumentor/action';
+
+enum MonitorMoment {
+  MonitorBefore = '01',
+  MonitorAfter = '02',
+}
+
+export interface MonitorWasmAddrJSONResponse {
+  interrupt: string;
+  kind: string;
+  error_code?: string;
+}
+export enum ResponseType {
+  SuccessResponse = '01',
+  ErrorResponse = '02',
+}
+
+function getResponseTypeFromString(str: string): ResponseType | undefined {
+  switch (str) {
+    case '01':
+      return ResponseType.SuccessResponse;
+    case '02':
+      return ResponseType.ErrorResponse;
+    default:
+      return undefined;
+  }
+}
+
+export interface MonitorWasmAddrResponse {
+  interrupt: Instruction;
+  responseType: ResponseType;
+  error_code?: number;
+}
+
+export function createMonitorWasmAddrResponse(
+  obj: MonitorWasmAddrJSONResponse,
+): MonitorWasmAddrResponse | undefined {
+  const instr = getInstructionFromString(obj.interrupt);
+  const responseType = getResponseTypeFromString(obj.kind);
+  if (
+    instr === undefined ||
+    responseType === undefined ||
+    instr !== Instruction.MonitorWasmAddr
+  ) {
+    return undefined;
+  }
+
+  const reply: MonitorWasmAddrResponse = {
+    interrupt: instr,
+    responseType,
+  };
+  if (obj.error_code !== undefined) {
+    const code = parseInt(obj.error_code);
+    if (!isNaN(code)) {
+      return undefined;
+    }
+    reply.error_code = code;
+  }
+
+  return reply;
+}
+export function isSuccessfulReply(reply: MonitorWasmAddrResponse): boolean {
+  return reply.responseType === ResponseType.SuccessResponse;
+}
+
+export function isMonitorWasmAddrResponse(
+  content: any,
+): content is MonitorWasmAddrJSONResponse {
+  const validFields =
+    typeof content === 'object' &&
+    typeof content.interrupt === 'string' &&
+    typeof content.kind === 'string' &&
+    (typeof content.error_code === 'string' ||
+      content.error_code === undefined);
+  if (validFields) {
+    return content.interrupt === Instruction.MonitorWasmAddr;
+  }
+  return false;
+}
+
+export class MontiroWasmAddrRequest
+  implements APIRequest<MonitorWasmAddrResponse>
+{
+  public readonly wasmAddr;
+  public readonly actions: InstrumentAction[];
+  private moment: MonitorMoment;
+  constructor(wasmAddr: number) {
+    this.wasmAddr = wasmAddr;
+    this.actions = [];
+    this.moment = MonitorMoment.MonitorBefore;
+  }
+
+  monitorBefore(): void {
+    this.moment = MonitorMoment.MonitorBefore;
+  }
+
+  monitorAfter(): void {
+    this.moment = MonitorMoment.MonitorAfter;
+  }
+
+  addAction(action: InstrumentAction): MontiroWasmAddrRequest {
+    if (this.actions.length === 0) {
+      this.actions.push(action);
+    } else {
+      getGlobalLogger().debug(
+        'Todo support multiple actions. For now just one action',
+      );
+    }
+    return this;
+  }
+
+  getData(): string {
+    const encodedAddr = encodeLEB128ToHex(this.wasmAddr);
+    const encodedSchedule = this.actions[0].schedule.serializeBinary();
+    const encodedAction = this.actions[0].serializeBinary();
+    return `${Instruction.MonitorWasmAddr}${encodedAddr}${this.moment}${encodedSchedule}${encodedAction}\n`;
+  }
+
+  parse(input: string): MonitorWasmAddrResponse {
+    const err = new APIRequestInvalidParse(
+      'No reply for AroundFunctionRequest',
+    );
+    const obj = JSON.parse(input);
+    if (isMonitorWasmAddrResponse(obj)) {
+      const reply = createMonitorWasmAddrResponse(obj);
+      if (reply === undefined) {
+        throw err;
+      } else {
+        return reply;
+      }
+    } else {
+      throw err;
+    }
+  }
+}
