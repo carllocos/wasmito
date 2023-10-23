2c574b611644b62d067a03ecab454b396669522c
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-23 17:44:38 +0200

    add inspect state payload generator

--- src/warduino/api/inspect_request.ts
+++ src/warduino/api/inspect_request.ts
@@ -5,6 +5,7 @@ import {
 } from './request_interface';
 import { WasmStack } from '../../state/wasm_stack';
 import { type WASM } from '../../state/wasm';
+import { serializeUInt16BE } from '../../util/encoder';
 
 enum InspectableState {
   pcState = '01',
@@ -35,6 +36,83 @@ export class InspectStack implements APIRequest<WasmStack> {
       return new WasmStack(resp.stack);
     } catch (e) {
       throw new APIRequestInvalidParse('No response for inspect stack');
+    }
+  }
+}
+
+export class StateRequest {
+  private readonly state: string[] = [];
+
+  public isRequestEmpty(): boolean {
+    return this.state.length === 0;
+  }
+
+  public includePC(): StateRequest {
+    this.pushState(InspectableState.pcState);
+    return this;
+  }
+
+  public includeStack(): StateRequest {
+    this.pushState(InspectableState.stackState);
+    return this;
+  }
+
+  public includeCallstack(): StateRequest {
+    this.pushState(InspectableState.callstackState);
+    return this;
+  }
+
+  public includeGlobals(): StateRequest {
+    this.pushState(InspectableState.globalsState);
+    return this;
+  }
+
+  public includeMemory(): StateRequest {
+    this.pushState(InspectableState.memState);
+    return this;
+  }
+
+  public includeTable(): StateRequest {
+    this.pushState(InspectableState.tableState);
+    return this;
+  }
+
+  public includeBranchingTable(): StateRequest {
+    this.pushState(InspectableState.branchingTableState);
+    return this;
+  }
+
+  public includeBreakpoints(): StateRequest {
+    this.pushState(InspectableState.breakpointState);
+    return this;
+  }
+
+  public includeError(): StateRequest {
+    this.pushState(InspectableState.errorState);
+    return this;
+  }
+
+  public includeCallbackMappings(): StateRequest {
+    this.pushState(InspectableState.callbacksState);
+    return this;
+  }
+
+  public includeEvents(): StateRequest {
+    this.pushState(InspectableState.eventsState);
+    return this;
+  }
+
+  public generateInterrupt(): string {
+    this.state.sort();
+    const numberBytes = serializeUInt16BE(this.state.length);
+    const stateToReq = this.state.join('');
+    return `${Instruction.Inspect}${numberBytes}${stateToReq}`;
+  }
+
+  private pushState(s: string): void {
+    const present = this.state.find((s2) => s === s2);
+    if (present === undefined) {
+      this.state.push(s);
     }
   }
 }
