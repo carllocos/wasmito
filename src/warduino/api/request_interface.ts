d87eccc48e124706558f5c9c48c49a04e446651b
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-24 14:58:23 +0200

    requests may be subscription requests

--- src/warduino/api/request_interface.ts
+++ src/warduino/api/request_interface.ts
@@ -3,6 +3,13 @@ export class APIRequestInvalidParse extends Error {}
 export interface APIRequest<R> {
   getData: () => string;
   parse: (input: string) => R;
+  handleSubscriptionData?: (data: string) => void;
+}
+
+export abstract class APISubscriptionRequest<R> implements APIRequest<R> {
+  abstract getData(): string;
+  abstract parse(input: string): R;
+  abstract handleSubscriptionData(data: string): void;
 }
 
 export enum Instruction {
