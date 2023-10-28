3a9586134aafeffc60ef72ac2ff0a8a3d00104fb
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-28 12:03:54 +0200

    add interrupt message interfaces

--- src/warduino/api/request_interface.ts
+++ src/warduino/api/request_interface.ts
@@ -1,3 +1,5 @@
+import { getInstructionFromString, type Instruction } from './instructions';
+
 export class APIRequestInvalidParse extends Error {}
 
 export interface APIRequest<R> {
@@ -10,4 +12,100 @@ export abstract class APISubscriptionRequest<R> implements APIRequest<R> {
   abstract getData(): string;
   abstract parse(input: string): R;
   abstract handleSubscriptionData(data: string): void;
+}
+
+export enum ResponseType {
+  SuccessResponse = '01',
+  ErrorResponse = '02',
+  SubscriptionResponse = '03',
+}
+
+export function getResponseTypeFromString(
+  str: string,
+): ResponseType | undefined {
+  switch (str) {
+    case '01':
+      return ResponseType.SuccessResponse;
+    case '02':
+      return ResponseType.ErrorResponse;
+    case '03':
+      return ResponseType.SubscriptionResponse;
+    default:
+      return undefined;
+  }
+}
+
+export interface ResponseJSONMessage {
+  interrupt: string;
+  kind: string;
+  error_code?: string;
+  sub?: any;
+}
+
+export interface RequestMessage {
+  interrupt: Instruction;
+  responseType: ResponseType;
+  error_code?: number;
+  sub?: any;
+}
+
+export function isSuccessfulMessage(reply: RequestMessage): boolean {
+  return reply.responseType === ResponseType.SuccessResponse;
+}
+
+export function isSubscriptionMessage(msg: RequestMessage): boolean {
+  return (
+    msg.responseType === ResponseType.SubscriptionResponse &&
+    msg.sub !== undefined &&
+    msg.error_code === undefined
+  );
+}
+
+function createMessageFromJSON(content: any): RequestMessage | undefined {
+  let obj: ResponseJSONMessage | undefined;
+  if (typeof content === 'string') {
+    try {
+      obj = JSON.parse(content);
+    } catch (e) {
+      return undefined;
+    }
+  }
+  if (obj === undefined) {
+    return undefined;
+  }
+  const instr = getInstructionFromString(obj.interrupt);
+  const responseType = getResponseTypeFromString(obj.kind);
+  if (instr === undefined || responseType === undefined) {
+    return undefined;
+  }
+
+  const response: RequestMessage = {
+    interrupt: instr,
+    responseType,
+  };
+
+  if (obj.error_code !== undefined) {
+    const errorCode = parseInt(obj.error_code);
+    if (
+      !isNaN(errorCode) ||
+      response.responseType !== ResponseType.ErrorResponse
+    ) {
+      return undefined;
+    }
+    response.error_code = errorCode;
+    return response;
+  } else if (obj.sub !== undefined) {
+    if (responseType !== ResponseType.SubscriptionResponse) {
+      return undefined;
+    }
+    response.sub = obj.sub;
+    return response;
+  } else if (responseType === ResponseType.SuccessResponse) {
+    return response;
+  }
+  return undefined;
+}
+
+export function createRequestMessage(obj: any): RequestMessage | undefined {
+  return createMessageFromJSON(obj);
 }
