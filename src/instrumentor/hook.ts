26d6ef137987a26be9b9909ac72637dd54117888
carllocos <carlosj.rojascastillo@gmail.com>
2023-11-10 23:04:24 +0100

    state hook parses missing state

--- src/instrumentor/hook.ts
+++ src/instrumentor/hook.ts
@@ -156,29 +156,90 @@ export class InspectState extends InstrumentHook<WasmState> {
       state.callstack = parsed.callstack;
     }
     if (parsed.table !== undefined) {
-      getGlobalLogger().error(
-        `TODO: handle the case where table is present in inspectedState`,
-      );
+      if (isNaN(parsed.table.max)) {
+        throw new Error(`Table max isNaN ${parsed.table.max}`);
+      }
+
+      if (isNaN(parsed.table.init)) {
+        throw new Error(`Table init isNaN ${parsed.table.init}`);
+      }
+      state.table = {
+        max: parsed.table.max,
+        init: parsed.table.init,
+        elements: parsed.table.elements.map((v: any) => {
+          if (isNaN(v)) {
+            throw new Error(`Table element isNaN ${v}`);
+          }
+          return v;
+        }),
+      };
     }
     if (parsed.memory !== undefined) {
-      getGlobalLogger().error(
-        `TODO: handle the case where memory is present in inspectedState`,
-      );
+      if (isNaN(parsed.memory.pages)) {
+        throw new Error(`Memory pages isNaN ${parsed.memory.pages}`);
+      }
+      if (isNaN(parsed.memory.max)) {
+        throw new Error(`Memory max isNaN ${parsed.memory.max}`);
+      }
+      if (isNaN(parsed.memory.init)) {
+        throw new Error(`Memory init isNaN ${parsed.memory.init}`);
+      }
+      const memBytes = Uint8Array.from(parsed.memory.bytes);
+      if (memBytes === undefined) {
+        throw new Error(`Memory bytes are invalid ${parsed.memory.bytes}`);
+      }
+      state.memory = {
+        pages: parsed.memory.pages,
+        max: parsed.memory.max,
+        init: parsed.memory.init,
+        bytes: memBytes,
+      };
     }
     if (parsed.br_table !== undefined) {
-      getGlobalLogger().error(
-        `TODO: handle the case where br_table is present in inspectedState`,
-      );
+      const brTableSize = parseInt(parsed.br_table.size, 16);
+      if (isNaN(brTableSize)) {
+        throw new Error(`Invalid BR_table size ${parsed.br_table.size}`);
+      }
+      state.br_table = {
+        size: brTableSize,
+        labels: parsed.br_table.labels.map((v: any) => {
+          if (isNaN(v)) {
+            throw new Error(`Invalid BR_table label is NaN ${v}`);
+          }
+          return v;
+        }),
+      };
     }
     if (parsed.callbacks !== undefined) {
-      getGlobalLogger().error(
-        `TODO: handle the case where callbacks are present in inspectedState`,
-      );
+      state.callbacks = parsed.callbacks.map((cb: any) => {
+        if (isNaN(cb.callbackid)) {
+          throw new Error(`Callback id is NaN ${cb.callbackid}`);
+        }
+        return {
+          callbackid: cb.callbackid,
+          tableIndexes: cb.tableIndexes.map((i: any) => {
+            if (isNaN(i)) {
+              throw new Error(`Callback index is NaN ${i}`);
+            }
+            return i;
+          }),
+        };
+      });
     }
     if (parsed.events !== undefined) {
-      getGlobalLogger().error(
-        `TODO: handle the case where events are present in inspectedState`,
-      );
+      state.events = parsed.events.map((ev: any) => {
+        if (typeof ev.topic !== 'string') {
+          throw new Error(`event topic should be string got ${ev.topic}`);
+        }
+
+        if (typeof ev.payload !== 'string') {
+          throw new Error(`event topic should be string got ${ev.topic}`);
+        }
+        return {
+          topic: ev.topic,
+          payload: ev.payload,
+        };
+      });
     }
     return state;
   }
