0faacef3752774aff5bdf46e870b51cadd02570e
carllocos <carlosj.rojascastillo@gmail.com>
2023-11-15 12:59:47 +0100

    add factory class

---
+++ src/builder/platformbuilder_factory.ts
@@ -0,0 +1,30 @@
+import { getGlobalLogger } from '../logger/logger';
+import { type PlatformBuilder } from './platformbuilder';
+import { Platform, type PlatformBuilderConfig } from './platform_config';
+import { ArduinoBoardBuilder } from './platforms/arduino_platform';
+
+export class BoardBuilderFactoryError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'BoardBuilderFactoryError';
+    Error.captureStackTrace(this, BoardBuilderFactoryError);
+  }
+}
+
+export function createPlatformBuilder(
+  platformConfig: PlatformBuilderConfig,
+  outputDir: string = '',
+): PlatformBuilder {
+  switch (platformConfig.platform) {
+    case Platform.Arduino:
+      getGlobalLogger().info(
+        `Creating Arduino Board Builder for ${platformConfig.deviceConfig.name}`,
+      );
+      return new ArduinoBoardBuilder(platformConfig, outputDir);
+    default: {
+      const errMsg = `Cannot create Unsupported Board Builder for ${platformConfig.deviceConfig.name}`;
+      getGlobalLogger().error(errMsg);
+      throw new BoardBuilderFactoryError(errMsg);
+    }
+  }
+}
