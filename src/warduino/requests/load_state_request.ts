a5ffd6dd5c4bd693421c95efa37572939bae15ba
carllocos <carlosj.rojascastillo@gmail.com>
2023-11-16 20:24:10 +0100

    add loadstate request

---
+++ src/warduino/requests/load_state_request.ts
@@ -0,0 +1,45 @@
+import { StateBinaryEncoder } from '../../state/old_binary_state_serializer';
+import { type WasmState } from '../../state/wasm';
+import {
+  APIRequestInvalidParse,
+  APIRequestNoSubscription,
+} from '../api/request_interface';
+
+export class LoadStateRequest extends APIRequestNoSubscription<string> {
+  private readonly encodedState: string;
+  private readonly lastRequest: boolean;
+
+  constructor(encodeState: string, lastRequest: boolean) {
+    super();
+    this.encodedState = encodeState;
+    this.lastRequest = lastRequest;
+  }
+
+  getData(): string {
+    return this.encodedState;
+  }
+
+  parse(input: string): string {
+    const expectedAck = this.lastRequest ? 'done!' : 'ack!';
+    if (input === expectedAck) {
+      return input;
+    }
+    throw new APIRequestInvalidParse('No ack for StateUpdate request');
+  }
+}
+
+export class LoadStateRequestBuilder {
+  private readonly encoder: StateBinaryEncoder;
+
+  constructor(wasmState: WasmState) {
+    this.encoder = new StateBinaryEncoder(wasmState);
+  }
+
+  buildRequests(): LoadStateRequest[] {
+    const binaryHexs = this.encoder.toBinary();
+    return binaryHexs.map((encodedState, idx) => {
+      const lastRequest = idx + 1 >= binaryHexs.length;
+      return new LoadStateRequest(encodedState, lastRequest);
+    });
+  }
+}
