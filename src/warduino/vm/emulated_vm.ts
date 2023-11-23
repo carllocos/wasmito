0da539eb3d49d46ef039c56910a14c323a123caf
carllocos <carlosj.rojascastillo@gmail.com>
2023-11-23 21:31:54 +0100

    moved process spawn to emulated vm

--- src/warduino/vm/emulated_vm.ts
+++ src/warduino/vm/emulated_vm.ts
@@ -1,4 +1,4 @@
-import { type ChildProcess } from 'child_process';
+import { spawn, type ChildProcess } from 'child_process';
 import { WARDuinoVM } from './warduino_vm';
 import { type EmulatorSpawnArguments } from '../../device/emulator_config';
 import { ClientSideSocket } from '../../communication/client_socket';
@@ -11,6 +11,7 @@ import {
   PlatformBuilderConfig,
 } from '../../builder/platform_config';
 import { UpdateWasmModuleRequest } from '../requests/update_module_request';
+import { getPath2WARDuinoSDKEmulatorBinary } from '../../project_config';
 
 export class EmulatedWARDuinoVMError extends Error {
   constructor(message: string) {
@@ -80,9 +81,76 @@ export class EmulatedWARDuinoVM extends WARDuinoVM {
       return false;
     }
 
-    const wasm = await this.platform.getWasm();
+    const sourceMap = this.platform.getSourceMap();
+    if (sourceMap === undefined) {
+      throw new EmulatedWARDuinoVMError(`SourceMap is undefined`);
+    }
+    const wasm = await sourceMap.getWasm();
     const updateRequest = new UpdateWasmModuleRequest(wasm);
     await this.sendRequest(updateRequest, timeout);
     return true;
+  }
+
+  public async spawn(): Promise<ChildProcess> {
+    const exitCode = await this.platform.compile(this.deviceConfig.program);
+    if (exitCode !== 0) {
+      throw new EmulatedWARDuinoVMError(
+        `Could not start emulator. Compilation exited with code: ${exitCode}`,
+      );
+    }
+    const sourceMap = this.platform.getSourceMap();
+    if (sourceMap === undefined) {
+      throw new EmulatedWARDuinoVMError(`Could not generate SourceMap`);
+    }
+    const processArgs = this.buildProcessArguments(
+      sourceMap.wasmFilePath,
+      this.args,
+    );
+    this.logger.info(
+      `starting emulator process with arguments ${processArgs.join(' ')}`,
+    );
+    const spawnCommand = getPath2WARDuinoSDKEmulatorBinary();
+    if (spawnCommand === undefined) {
+      throw new EmulatedWARDuinoVMError(
+        "Path to WARDuino SDK is not set. You can set it via env variable 'WARDUINO_SDK=PATH'",
+      );
+    }
+
+    this.logger.debug(
+      'decide whether to move callbacks on data or on close to the EmulateDevice class',
+    );
+
+    const childProcess = spawn(spawnCommand, processArgs);
+    childProcess.stdout.on('data', (data) => {
+      this.logger.debug(`${this.deviceConfig.name} (Spawned process): ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+      this.logger.error(`${this.deviceConfig.name} (Spawned process): ${data}`);
+    });
+
+    return childProcess;
+  }
+
+  private buildProcessArguments(
+    programPath: string,
+    args: EmulatorSpawnArguments,
+  ): string[] {
+    const processArgs: string[] = [programPath];
+
+    if (args.listenPort !== undefined) {
+      processArgs.push('--socket');
+      processArgs.push(args.listenPort.toString());
+    }
+    if (args.pauseOnStart) {
+      processArgs.push('--paused');
+    }
+    if (
+      args.disableStrictModuleLoad !== undefined &&
+      args.disableStrictModuleLoad
+    ) {
+      processArgs.push('--disable-strict-module-load');
+    }
+    return processArgs;
   }
 }
