dfdb9c182dbced5da8f85da89b4325a52dbba259
carllocos <carlosj.rojascastillo@gmail.com>
2023-12-11 12:06:23 +0100

    expose hooks via api

--- src/warduino/api/warduino_api.ts
+++ src/warduino/api/warduino_api.ts
@@ -1,3 +1,4 @@
+import { type Hook } from '../../hooks/hook';
 import { type WasmState } from '../../state/wasm';
 import { type StateRequest } from '../requests/inspect_request';
 
@@ -5,9 +6,22 @@ export interface WARDuinoAPI {
   connect: (timeout?: number) => Promise<boolean>;
   disconnect: (timeout?: number) => Promise<boolean>;
   close: () => Promise<boolean>;
+
   run: (timeout?: number) => Promise<boolean>;
+
   pause: (timeout?: number) => Promise<void>;
+
   step: (timeout?: number) => Promise<void>;
+
+  addBreakpoint: (
+    sourceCodeLocation: number,
+    timeout?: number,
+  ) => Promise<boolean>;
+
+  removeBreakpoint: (
+    sourceCodeLocation: number,
+    timeout?: number,
+  ) => Promise<boolean>;
 
   proxify: (timeout?: number) => Promise<void>;
 
@@ -23,4 +37,17 @@ export interface WARDuinoAPI {
   loadWasmState: (state: WasmState, timeout?: number) => Promise<void>;
 
   resolveEvent: (timeout?: number) => Promise<void>;
+
+  // Hook API
+  addHookBefore: <T>(
+    sourceCodeLocation: number,
+    hook: Hook<T>,
+    timeout?: number,
+  ) => Promise<boolean>;
+
+  addHookAfter: <T>(
+    sourceCodeLocation: number,
+    hook: Hook<T>,
+    timeout?: number,
+  ) => Promise<boolean>;
 }
