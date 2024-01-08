98c18f5ca58ee03dd7df7abce6c47e5a440ccc05
carllocos <carlosj.rojascastillo@gmail.com>
2024-01-08 10:00:35 +0100

    add empty channel

---
+++ src/communication/no_channel.ts
@@ -0,0 +1,40 @@
+import { type Channel } from './channel_interface';
+
+/*
+ * NoChannel is a class that throws errors for each member of the Channel interface.
+ * The class is only used as a temporary channel and is meant to be replaced with another channel
+ * where the other channel represents a real connection.
+ * In some cases, a VM has no channel yet and therefore NoChannel makes the transition to a real channel easier.
+ */
+
+export class NoChannel implements Channel {
+  async open(timeout?: number | undefined): Promise<boolean> {
+    throw Error(
+      'NoChannel has no implementation and serves only as transition channel',
+    );
+  }
+
+  async close(): Promise<boolean> {
+    throw Error(
+      'NoChannel has no implementation and serves only as transition channel',
+    );
+  }
+
+  async send(data: string): Promise<boolean> {
+    throw Error(
+      'NoChannel has no implementation and serves only as transition channel',
+    );
+  }
+
+  addOnData(callback: (data: string) => void): void {
+    throw Error(
+      'NoChannel has no implementation and serves only as transition channel',
+    );
+  }
+
+  removeOnData(callback: (data: string) => void): void {
+    throw Error(
+      'NoChannel has no implementation and serves only as transition channel',
+    );
+  }
+}
