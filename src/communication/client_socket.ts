023d3bb7f22e923bd2538164af4b8c311e227f63
carllocos <carlosj.rojascastillo@gmail.com>
2024-01-09 19:56:56 +0100

    moved abstrac class to seperate file

--- src/communication/client_socket.ts
+++ src/communication/client_socket.ts
@@ -1,77 +1,7 @@
 import * as net from 'net';
-import { type Channel } from '../communication/channel_interface';
 import { getGlobalLogger } from '../logger/logger';
 import { waitForPortToBeUsed } from '../util/socket_util';
-
-abstract class AbstractChannel implements Channel {
-  private readonly channelName: string;
-  protected connection?: net.Socket;
-  private dataBuffered: string = '';
-  private listeners: Array<(data: string) => void>;
-  private readonly removedListeners: Set<(data: string) => void>;
-
-  constructor(channelName: string) {
-    this.channelName = channelName;
-    this.listeners = [];
-    this.removedListeners = new Set();
-  }
-
-  // Abstract methods
-  public abstract write(
-    data: string,
-    cb?: ((err?: Error | undefined) => void) | undefined,
-  ): boolean;
-
-  public abstract open(timeout?: number): Promise<boolean>;
-
-  public abstract close(): Promise<boolean>;
-
-  public abstract send(data: string): Promise<boolean>;
-
-  public addOnData(callback: (data: string) => void): void {
-    this.listeners.push(callback);
-  }
-
-  public removeOnData(callback: (data: string) => void): void {
-    this.removedListeners.add(callback);
-  }
-
-  protected onDataHandler(data: Buffer): void {
-    this.dataBuffered += data.toString();
-    this.handleLines(this.parseLines());
-    this.listeners = this.listeners.filter((cb) => {
-      return !this.removedListeners.has(cb);
-    });
-    this.removedListeners.clear();
-  }
-
-  private handleLines(lines: string[]): void {
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      this.listeners.forEach((listener) => {
-        if (!this.removedListeners.has(listener)) {
-          listener(line);
-        }
-      });
-    }
-  }
-
-  private parseLines(): string[] {
-    const lines = [];
-    let idx = this.dataBuffered.indexOf('\n');
-    while (idx !== -1) {
-      let line = this.dataBuffered.slice(0, idx);
-      this.dataBuffered = this.dataBuffered.slice(idx + 1); // skip newline
-      if (line.length > 0 && line.charAt(line.length - 1) === '\r') {
-        line = line.slice(0, line.length - 1);
-      }
-      getGlobalLogger().debug(`${this.channelName}: ${line}`);
-      lines.push(line);
-      idx = this.dataBuffered.indexOf('\n');
-    }
-    return lines;
-  }
-}
+import { AbstractChannel } from './abstract_channel';
 
 export class ClientSideSocket extends AbstractChannel {
   private readonly port: number;
