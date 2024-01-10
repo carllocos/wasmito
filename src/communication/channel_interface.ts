b941424177c2ab90b3d613250bc1de71a91c19aa
carllocos <carlosj.rojascastillo@gmail.com>
2024-01-10 11:56:36 +0100

    made write part of interface + change write signature

--- src/communication/channel_interface.ts
+++ src/communication/channel_interface.ts
@@ -1,6 +1,10 @@
 export interface Channel {
   open: (timeout?: number) => Promise<boolean>;
   close: () => Promise<boolean>;
+  write: (
+    data: any,
+    cb?: ((err?: Error | null | undefined) => void) | undefined,
+  ) => boolean;
   send: (data: string) => Promise<boolean>;
   addOnData: (callback: (data: string) => void) => void;
   removeOnData: (callback: (data: string) => void) => void;
