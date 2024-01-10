b301023a3a6c919482c5a350f2ca89dac369149a
carllocos <carlosj.rojascastillo@gmail.com>
2024-01-10 16:01:22 +0100

    removed unneeded fields not part of api

--- src/warduino/api/warduino_api.ts
+++ src/warduino/api/warduino_api.ts
@@ -7,10 +7,6 @@ import { type WasmState } from '../../state/wasm';
 import { type StateRequest } from '../requests/inspect_request';
 
 export interface WARDuinoAPI {
-  connect: (timeout?: number) => Promise<boolean>;
-  disconnect: (timeout?: number) => Promise<boolean>;
-  close: () => Promise<boolean>;
-
   run: (timeout?: number) => Promise<boolean>;
 
   pause: (timeout?: number) => Promise<void>;
