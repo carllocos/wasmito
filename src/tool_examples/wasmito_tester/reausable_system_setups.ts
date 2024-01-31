158034bb8dcc4fe2ca257489fd8a62de382b6e7e
carllocos <23368410+carllocos@users.noreply.github.com>
2024-01-31 19:21:54 +0100

    [WIP] add reusable test code

---
+++ src/tool_examples/wasmito_tester/reausable_system_setups.ts
@@ -0,0 +1,52 @@
+import {
+  type PostSetupConfig,
+  type DeviceSetup,
+  type SystemSetup,
+} from './system_deployer';
+
+export function oneM5StickCMCU(
+  program: string,
+  id: string,
+  postSetup?: PostSetupConfig,
+): DeviceSetup {
+  const ps: PostSetupConfig = {
+    pauseAfterSetup: true,
+  };
+  const OneM5StickCMCU: DeviceSetup = {
+    program,
+    target: 'mcu',
+    name: 'mcu m5stickc',
+    id,
+    fqbn: 'esp32:esp32:m5stick-c',
+    postSetup: postSetup ?? ps,
+  };
+  return OneM5StickCMCU;
+}
+
+export function oneM5StickCDev(
+  program: string,
+  id: string,
+  postSetup?: PostSetupConfig,
+): DeviceSetup {
+  const ps: PostSetupConfig = {
+    pauseAfterSetup: true,
+  };
+  const dev: DeviceSetup = {
+    program,
+    target: 'dev',
+    name: 'm5stickc Dev',
+    id,
+    postSetup: postSetup ?? ps,
+  };
+  return dev;
+}
+
+export function createSystemSetup(
+  setupName: string,
+  devices: DeviceSetup[],
+): SystemSetup {
+  return {
+    setupName,
+    devices,
+  };
+}
