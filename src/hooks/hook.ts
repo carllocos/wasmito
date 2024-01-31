b2c53b35c290c47e16e8e3423ad81eb3522b967e
carllocos <23368410+carllocos@users.noreply.github.com>
2024-01-31 18:55:56 +0100

    refactor hook to no longer be generic + add subscriptionhook abstract class

--- src/hooks/hook.ts
+++ src/hooks/hook.ts
@@ -12,9 +12,20 @@ export enum HookKind {
   StateToInspect = '03',
   ChangeRunningState = '04',
   ProxyCall = '05',
+
+  // event hooks from 0x10 to 0x1f
+  EventInspect = '10',
+  EventRemove = '11',
 }
 
-export abstract class Hook<SubscriptionType> {
+export interface SubscriptionHook<SubscriptionType> {
+  readonly subscribe: (calllback: (value: SubscriptionType) => void) => void;
+  readonly unSubscribe: (calllback: (value: SubscriptionType) => void) => void;
+  readonly onSubscriptionData: (data: SubscriptionType) => void;
+  readonly parseSubscriptionData: (input: any) => SubscriptionType;
+}
+
+export abstract class Hook {
   public readonly kind: HookKind;
   public schedule: HookSchedule;
   constructor(kind: HookKind) {
@@ -22,12 +33,12 @@ export abstract class Hook<SubscriptionType> {
     this.schedule = new ScheduleAways();
   }
 
-  scheduleFor(newSchedule: HookSchedule): Hook<SubscriptionType> {
+  scheduleFor(newSchedule: HookSchedule): Hook {
     this.schedule = newSchedule;
     return this;
   }
 
-  scheduleOnce(logicalClock?: LogicalClock): Hook<SubscriptionType> {
+  scheduleOnce(logicalClock?: LogicalClock): Hook {
     if (logicalClock !== undefined) {
       this.schedule = new ScheduleOnTimeStamp(logicalClock);
     } else {
@@ -37,10 +48,43 @@ export abstract class Hook<SubscriptionType> {
   }
 
   abstract description(): string;
-
   abstract serializeBinary(): string;
-  parseSubscriptionData?: (input: any) => SubscriptionType;
-  onSubscriptionData?: (data: SubscriptionType) => void;
 }
 
-export abstract class HookWithoutSubscription extends Hook<void> {}
+export abstract class HookWithoutSubscription extends Hook {}
+
+export abstract class HookWithSubscription<SubscriptionType>
+  extends Hook
+  implements SubscriptionHook<SubscriptionType>
+{
+  private listeners: Array<(data: SubscriptionType) => void>;
+  private readonly removedListeners: Set<(data: SubscriptionType) => void>;
+
+  constructor(kind: HookKind) {
+    super(kind);
+    this.listeners = [];
+    this.removedListeners = new Set();
+  }
+
+  public subscribe(callback: (data: SubscriptionType) => void): void {
+    this.listeners.push(callback);
+  }
+
+  public unSubscribe(callback: (data: SubscriptionType) => void): void {
+    this.removedListeners.add(callback);
+  }
+
+  onSubscriptionData(value: SubscriptionType): void {
+    this.listeners.forEach((listener) => {
+      if (!this.removedListeners.has(listener)) {
+        listener(value);
+      }
+    });
+    this.listeners = this.listeners.filter((cb) => {
+      return !this.removedListeners.has(cb);
+    });
+    this.removedListeners.clear();
+  }
+
+  abstract parseSubscriptionData(input: any): SubscriptionType;
+}
