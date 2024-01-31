e65a1289e3ef626c48ac0c7f0cf06106b69ee238
carllocos <23368410+carllocos@users.noreply.github.com>
2024-01-31 18:51:36 +0100

    add custom timeout error + help function

--- src/util/promise_util.ts
+++ src/util/promise_util.ts
@@ -1,3 +1,11 @@
+export class TimeoutPromise extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'TimeoutPromise';
+    Error.captureStackTrace(this, TimeoutPromise);
+  }
+}
+
 export async function timeoutPromise<T>(
   promise: Promise<T>,
   timeout: number,
@@ -6,7 +14,9 @@ export async function timeoutPromise<T>(
   return await new Promise<T>((resolve, reject) => {
     const timeoutId = setTimeout(() => {
       clearTimeout(timeoutId);
-      reject(error ?? new Error('Promise timed out'));
+      reject(
+        error ?? new TimeoutPromise(`Promise timed out after ${timeout} ms`),
+      );
     }, timeout);
 
     promise
@@ -18,5 +28,19 @@ export async function timeoutPromise<T>(
         clearTimeout(timeoutId);
         reject(error);
       });
+  });
+}
+
+export async function maybeTimeoutPromise<T>(
+  promise: Promise<T>,
+  timeout?: number,
+  error?: Error,
+): Promise<T> {
+  return await new Promise<T>((resolve, reject) => {
+    if (timeout === undefined) {
+      promise.then(resolve).catch(reject);
+    } else {
+      timeoutPromise(promise, timeout, error).then(resolve).catch(reject);
+    }
   });
 }
