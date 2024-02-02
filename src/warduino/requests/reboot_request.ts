8d7b4bc67fbb0690447d3a8090c3abbad949a49f
carllocos <23368410+carllocos@users.noreply.github.com>
2024-02-02 22:18:08 +0100

    [WIP] rebootrequest

---
+++ src/warduino/requests/reboot_request.ts
@@ -0,0 +1,21 @@
+import {
+  APIRequestInvalidParse,
+  APIRequestNoSubscription,
+} from '../api/request_interface';
+
+export class RebootRequest extends APIRequestNoSubscription<void> {
+  description(): string {
+    return 'RebootRequest';
+  }
+
+  getData(): string {
+    throw new Error('TODO');
+  }
+
+  parse(input: string): void {
+    if (input === 'Reboot!') {
+      return;
+    }
+    throw new APIRequestInvalidParse('No ack for Reboot');
+  }
+}
