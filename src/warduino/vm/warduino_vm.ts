a37c3ef2d9c33bb982f0523c25124f2407b7f6eb
carllocos <23368410+carllocos@users.noreply.github.com>
2024-03-11 10:43:48 +0100

    rename file

--- src/warduino/vm/warduino_vm.ts
+++ src/warduino/vm/warduino_vm.ts
@@ -10,7 +10,7 @@ import {
 } from '../api/request_interface';
 import { Command } from '../../communication/command';
 import { type PlatformBuilderConfig } from '../../builder/platform_config';
-import { type Platform } from '../../builder/platformbuilder';
+import { type Platform } from '../../builder/platform';
 import { createPlatformBuilder } from '../../builder/platformbuilder_factory';
 import { PauseRequest } from '../requests/pause_request';
 import { ProxifyRequest } from '../requests/proxify_request';
