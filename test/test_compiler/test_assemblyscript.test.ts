3d866745e54b56b79f4425ef2f7a50885e0cd039
Carlos Rojas <carlosj.rojascastillo@gmail.com>
2024-03-13 13:54:04 +0100

    comment test as it uses absolute paths

--- test/test_compiler/test_assemblyscript.test.ts
+++ test/test_compiler/test_assemblyscript.test.ts
@@ -1,26 +1,26 @@
-import assert from 'assert';
-import {
-  AssemblyScriptCompiler,
-  type AssemblyScriptCompilerArgs,
-} from '../../src/source_mappers/compilers/assemblyscript_compiler';
-import { type AssemblyScriptSourceMap } from '../../src/source_mappers/assemblyscript/assembly_script_source_map';
+// import assert from 'assert';
+// import {
+//   AssemblyScriptCompiler,
+//   type AssemblyScriptCompilerArgs,
+// } from '../../src/source_mappers/compilers/assemblyscript_compiler';
+// import { type AssemblyScriptSourceMap } from '../../src/source_mappers/assemblyscript/assembly_script_source_map';
 
-describe('Test AS Compilation', () => {
-  it('', async () => {
-    const outputDir = '/home/carllocos/Projects/wasmito/test/as_output/';
-    const config: AssemblyScriptCompilerArgs = {
-      pathToASConfig: '/home/carllocos/Projects/dimmer-m5stickc/asconfig.json',
-      pathToSrcRoot: '/home/carllocos/Projects/dimmer-m5stickc/',
-    };
-    const compiler = await AssemblyScriptCompiler.createCompiler(
-      config,
-      outputDir,
-    );
+// describe('Test AS Compilation', () => {
+//   it('', async () => {
+//     const outputDir = '/home/carllocos/Projects/wasmito/test/as_output/';
+//     const config: AssemblyScriptCompilerArgs = {
+//       pathToASConfig: '/home/carllocos/Projects/dimmer-m5stickc/asconfig.json',
+//       pathToSrcRoot: '/home/carllocos/Projects/dimmer-m5stickc/',
+//     };
+//     const compiler = await AssemblyScriptCompiler.createCompiler(
+//       config,
+//       outputDir,
+//     );
 
-    const sourceMap: AssemblyScriptSourceMap = await compiler.compile(
-      config.pathToASConfig,
-      outputDir,
-    );
-    assert(sourceMap !== undefined, 'failed');
-  });
-});
+//     const sourceMap: AssemblyScriptSourceMap = await compiler.compile(
+//       config.pathToASConfig,
+//       outputDir,
+//     );
+//     assert(sourceMap !== undefined, 'failed');
+//   });
+// });
