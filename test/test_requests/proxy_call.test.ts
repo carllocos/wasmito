c32c955c713428b21a273aa0ec9bd48c50fb0f58
Carlos Rojas <carlosj.rojascastillo@gmail.com>
2024-03-13 13:53:40 +0100

    uncomment test + refactor to use new OOP constructors

--- test/test_requests/proxy_call.test.ts
+++ test/test_requests/proxy_call.test.ts
@@ -1,149 +1,151 @@
-// import { DeviceManager } from '../../src/device/device_manager';
-// import {
-//   InputMode,
-//   type OutOfPlaceVM,
-// } from '../../src/warduino/vm/outofplace_vm';
-// import { expect } from 'chai';
-// import { MockVM } from '../shared/mock_vm';
-// import { type MockChannel } from '../shared/mock_channel';
-// import {
-//   ProxyCallRequest,
-//   isProxyCallSuccessfulResponse,
-// } from '../../src/warduino/requests/fun_call_request';
-// import { WasmValuesBuilder } from '../../src/state/wasm_value_array_builder';
-// import { timeoutPromise } from '../../src/util/promise_util';
-// import {
-//   type ProgLangSelectionArgs,
-//   TargetLanguage,
-// } from '../../src/source_mappers/compilers/prog_language_selection';
+import { DeviceManager } from '../../src/device/device_manager';
+import {
+  InputMode,
+  type OutOfPlaceVM,
+} from '../../src/warduino/vm/outofplace_vm';
+import { expect } from 'chai';
+import { MockVM } from '../shared/mock_vm';
+import { type MockChannel } from '../shared/mock_channel';
+import {
+  ProxyCallRequest,
+  isProxyCallSuccessfulResponse,
+} from '../../src/warduino/requests/fun_call_request';
+import { WasmValuesBuilder } from '../../src/state/wasm_value_array_builder';
+import { timeoutPromise } from '../../src/util/promise_util';
+import { TargetLanguage } from '../../src/source_mappers/compilers/prog_language_selection';
+import { type WATCompilerArgs, createDevPlatform } from '../../src';
 
-// /*
-//  * TODO: The tests prefixed with `integration:` are integration tests and therefore may need a different framework
-//  */
+/*
+ * TODO: The tests prefixed with `integration:` are integration tests and therefore may need a different framework
+ */
 
-// async function checkIfMessageReceived(
-//   expectedData: string,
-//   vm: OutOfPlaceVM,
-//   mockChannel: MockChannel,
-//   runVM?: boolean,
-// ): Promise<boolean> {
-//   return new Promise<boolean>((resolve, reject) => {
-//     mockChannel.addMockWriteMethod((dataWrittenToTargetVM: any): boolean => {
-//       const requestData = dataWrittenToTargetVM.toString();
-//       if (expectedData === requestData) {
-//         resolve(true);
-//       }
-//       return true;
-//     });
-//     if (runVM === undefined || runVM) {
-//       vm.run()
-//         .then((running) => {
-//           if (!running) {
-//             resolve(false);
-//           }
-//         })
-//         .catch((err: any) => {
-//           reject(err);
-//         });
-//     }
-//   });
-// }
+async function checkIfMessageReceived(
+  expectedData: string,
+  vm: OutOfPlaceVM,
+  mockChannel: MockChannel,
+  runVM?: boolean,
+): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    mockChannel.addMockWriteMethod((dataWrittenToTargetVM: any): boolean => {
+      const requestData = dataWrittenToTargetVM.toString();
+      if (expectedData === requestData) {
+        resolve(true);
+      }
+      return true;
+    });
+    if (runVM === undefined || runVM) {
+      vm.run()
+        .then((running) => {
+          if (!running) {
+            resolve(false);
+          }
+        })
+        .catch((err: any) => {
+          reject(err);
+        });
+    }
+  });
+}
 
-// describe('Intergation Test: Proxy Calls to a Mocked Target VM produces the right payloads', () => {
-//   it('Proxy Call a primitive function with 3 arguments', async () => {
-//     const deviceManager = new DeviceManager();
-//     const program = './test/data/test-example-proxy-call.wat';
-//     const selectLang: ProgLangSelectionArgs = {
-//       targetLanguage: TargetLanguage.WAT,
-//       compilerArgs: program,
-//     };
-//     const targetVM = new MockVM(selectLang);
-//     const uploaded = await targetVM.uploadSourceCode(program);
-//     expect(uploaded).to.be.equal(true);
+describe('Intergation Test: Proxy Calls to a Mocked Target VM produces the right payloads', () => {
+  it('Proxy Call a primitive function with 3 arguments', async () => {
+    const deviceManager = new DeviceManager();
+    const platform = await createDevPlatform({
+      selectedLanguage: {
+        targetLanguage: TargetLanguage.WAT,
+      },
+    });
+    const targetVM = new MockVM(platform);
 
-//     targetVM.mockResponseForAddHookOnNewEvent(true);
-//     targetVM.mockResponseForAddHookOnNewEvent(true);
-//     await targetVM.mockSnapshot('./test/data/proxy-call-snapshot-1.json');
-//     const proxyVM = await deviceManager.spawnOutOfPlaceVM(
-//       targetVM,
-//       InputMode.RedirectInput,
-//       8000,
-//     );
-//     const chipPinModeID = 1;
-//     const argsLine39 = new WasmValuesBuilder().addI32Value(39).addI32Value(5);
-//     const proxyCallLine39 = new ProxyCallRequest(
-//       chipPinModeID,
-//       argsLine39.values,
-//     );
-//     const proxyCallOfLine39RequestReceived = await timeoutPromise(
-//       checkIfMessageReceived(
-//         proxyCallLine39.encodeRemoteCallRequest(),
-//         proxyVM,
-//         targetVM.mockChannel,
-//       ),
-//       3000,
-//     );
-//     expect(proxyCallOfLine39RequestReceived).to.be.equal(true);
+    const sourceCodeCompilationArgs: WATCompilerArgs = {
+      sourceCodePath: './test/data/test-example-proxy-call.wat',
+    };
+    const uploaded = await targetVM.uploadSourceCode(sourceCodeCompilationArgs);
+    expect(uploaded).to.be.equal(true);
 
-//     const responseToRequest = '41010100\n';
-//     targetVM.mockChannel.mockOnData(responseToRequest);
+    targetVM.mockResponseForAddHookOnNewEvent(true);
+    targetVM.mockResponseForAddHookOnNewEvent(true);
+    await targetVM.mockSnapshot('./test/data/proxy-call-snapshot-1.json');
+    const proxyVM = await deviceManager.spawnOutOfPlaceVM(
+      targetVM,
+      InputMode.RedirectInput,
+      8000,
+    );
+    const chipPinModeID = 1;
+    const argsLine39 = new WasmValuesBuilder().addI32Value(39).addI32Value(5);
+    const proxyCallLine39 = new ProxyCallRequest(
+      chipPinModeID,
+      argsLine39.values,
+    );
+    const proxyCallOfLine39RequestReceived = await timeoutPromise(
+      checkIfMessageReceived(
+        proxyCallLine39.encodeRemoteCallRequest(),
+        proxyVM,
+        targetVM.mockChannel,
+      ),
+      3000,
+    );
+    expect(proxyCallOfLine39RequestReceived).to.be.equal(true);
 
-//     // After replying to proxy call the VM should continue the computation which will lead to the next proxyCall
-//     const subscribeInterruptID = 2;
-//     const argsLine44 = new WasmValuesBuilder()
-//       .addI32Value(39)
-//       .addI32Value(1)
-//       .addI32Value(2);
-//     const proxyCallLine44 = new ProxyCallRequest(
-//       subscribeInterruptID,
-//       argsLine44.values,
-//     );
+    const responseToRequest = '41010100\n';
+    targetVM.mockChannel.mockOnData(responseToRequest);
 
-//     const proxyCallOfLine44RequestReceived = await timeoutPromise(
-//       checkIfMessageReceived(
-//         proxyCallLine44.encodeRemoteCallRequest(),
-//         proxyVM,
-//         targetVM.mockChannel,
-//       ),
-//       3000,
-//     );
-//     expect(proxyCallOfLine44RequestReceived).to.be.equal(true);
-//     const closed = await deviceManager.closeVM(proxyVM, 3000);
-//     expect(closed).to.be.equal(true);
-//   });
+    // After replying to proxy call the VM should continue the computation which will lead to the next proxyCall
+    const subscribeInterruptID = 2;
+    const argsLine44 = new WasmValuesBuilder()
+      .addI32Value(39)
+      .addI32Value(1)
+      .addI32Value(2);
+    const proxyCallLine44 = new ProxyCallRequest(
+      subscribeInterruptID,
+      argsLine44.values,
+    );
 
-//   it.skip('Proxy Call a primitive function with no arguments', () => {});
-//   it.skip('Proxy Call a non-primitive function that does not exist', async () => {});
-//   it.skip('Proxy Call a function that takes no arguments', async () => {});
-//   it.skip('Proxy Call a function that takes 3 arguments', async () => {});
-// });
+    const proxyCallOfLine44RequestReceived = await timeoutPromise(
+      checkIfMessageReceived(
+        proxyCallLine44.encodeRemoteCallRequest(),
+        proxyVM,
+        targetVM.mockChannel,
+      ),
+      3000,
+    );
+    expect(proxyCallOfLine44RequestReceived).to.be.equal(true);
+    const closed = await deviceManager.closeVM(proxyVM, 3000);
+    expect(closed).to.be.equal(true);
+  });
 
-// describe('Intergation Test: Proxy Call handled by a target VM produces expected responses', () => {
-//   it('Respond to a Proxy Call of a function that does not return a value', async () => {
-//     const program = './test/data/test-example-proxy-call.wat';
-//     const selectedLang: ProgLangSelectionArgs = {
-//       targetLanguage: TargetLanguage.WAT,
-//       compilerArgs: program,
-//     };
-//     const deviceManager = new DeviceManager();
-//     const vm = await deviceManager.spawnDevelopmentVM(
-//       {
-//         program,
-//         disableStrictModuleLoad: true,
-//       },
-//       selectedLang,
-//       3000,
-//     );
+  it.skip('Proxy Call a primitive function with no arguments', () => {});
+  it.skip('Proxy Call a non-primitive function that does not exist', async () => {});
+  it.skip('Proxy Call a function that takes no arguments', async () => {});
+  it.skip('Proxy Call a function that takes 3 arguments', async () => {});
+});
 
-//     const chipPinModeID = 1;
-//     const args = new WasmValuesBuilder().addI32Value(39).addI32Value(5);
-//     const reply = await vm.proxyCall(chipPinModeID, args.values, 3000);
-//     expect(isProxyCallSuccessfulResponse(reply)).to.equal(true);
-//     const closed = await deviceManager.closeVM(vm, 3000);
-//     expect(closed).to.be.equal(true);
-//   });
+describe('Intergation Test: Proxy Call handled by a target VM produces expected responses', () => {
+  it('Respond to a Proxy Call of a function that does not return a value', async () => {
+    const platform = await createDevPlatform({
+      selectedLanguage: {
+        targetLanguage: TargetLanguage.WAT,
+      },
+    });
+    const sourceCodeCompilationArgs: WATCompilerArgs = {
+      sourceCodePath: './test/data/test-example-proxy-call.wat',
+    };
+    const deviceManager = new DeviceManager();
+    const vm = await deviceManager.spawnDevelopmentVM(
+      platform,
+      sourceCodeCompilationArgs,
+      3000,
+    );
 
-//   it.skip('Respond to a Proxy Call of a function that produces one return value', async () => {});
-//   it.skip('Respond to a Proxy Call of a function that produces an exception', async () => {});
-//   it.skip('Respond to a Proxy Call of a function that produces an exception', async () => {});
-// });
+    const chipPinModeID = 1;
+    const args = new WasmValuesBuilder().addI32Value(39).addI32Value(5);
+    const reply = await vm.proxyCall(chipPinModeID, args.values, 3000);
+    expect(isProxyCallSuccessfulResponse(reply)).to.equal(true);
+    const closed = await deviceManager.closeVM(vm, 3000);
+    expect(closed).to.be.equal(true);
+  });
+
+  it.skip('Respond to a Proxy Call of a function that produces one return value', async () => {});
+  it.skip('Respond to a Proxy Call of a function that produces an exception', async () => {});
+  it.skip('Respond to a Proxy Call of a function that produces an exception', async () => {});
+});
