3e83dde5657a18b8f071838d637e8e6e7ed0220a
Carlos Rojas <23368410+carllocos@users.noreply.github.com>
2024-03-28 11:09:50 +0100

    implement new abstract class interface

--- src/source_mappers/wat/wat_source_map.ts
+++ src/source_mappers/wat/wat_source_map.ts
@@ -1,98 +1,103 @@
-import { type WasmType } from '../../state/opcode_type';
-import { type VariableInfo } from '../parsers/obj-dump_parser';
+import { type LineInfoPairs } from '../parsers/obj-dump_parser';
 import {
   type SourceCodeMapping,
   SourceMap,
-  type WASMFunction,
+  type SourceCodeLocation,
 } from '../source_map';
-import { type WasmOpcode } from './opcodes';
+import { type WASMFunction } from '../wasm/wasm_function';
 
+// TODO Move to LangaugeAdaptor which will consist of
+// 1. SourceMap Or Dwarf
+// 2. AST or something in the kind
+// 3. Mapper to VM apps
 export class WATSourceMap extends SourceMap {
-  public readonly functions: WASMFunction[];
-  public readonly imports: WASMFunction[];
-  public readonly globals: VariableInfo[];
-
-  constructor(
-    sourcePath: string,
-    wasmPath: string,
-    types: WasmType[],
-    globals: VariableInfo[],
-    functions: WASMFunction[],
-    imported: WASMFunction[],
-  ) {
+  private readonly lineNrToInfo: Map<number, SourceCodeMapping[]>;
+  private readonly _sourceCodePath: string;
+  private readonly _mappings: SourceCodeMapping[];
+  constructor(lines: LineInfoPairs[], sourcePath: string, wasmPath: string) {
     super([sourcePath], wasmPath);
-    this.functions = functions;
-    this.imports = imported;
-    this.globals = globals;
+    this.lineNrToInfo = this.createWAT2WASMMappings(lines);
+    this._sourceCodePath = sourcePath;
+    this._mappings = this.createArrayFromMappings(this.lineNrToInfo);
   }
 
-  public getSources(): string[] {
-    // TODO fix
-    return [this.sourceCodeFilePath];
+  public generatedPositionFor(
+    location: SourceCodeLocation,
+  ): SourceCodeMapping[] {
+    const lines = this.lineNrToInfo.get(location.linenr);
+    if (lines === undefined) {
+      return [];
+    }
+
+    const columnStart = location.columnStart;
+    const columnEnd = location.columnEnd;
+    if (columnStart === undefined && columnEnd !== undefined) {
+      throw new Error('StartColumn expected');
+    }
+    return lines.filter((l) => {
+      return columnStart === undefined || l.columnStart <= columnStart;
+    });
+  }
+
+  getOriginalPositionFor(wasmAddr: number): SourceCodeMapping | undefined {
+    return this._mappings.find((m: SourceCodeMapping) => {
+      return m.address === wasmAddr;
+    });
   }
 
   public getFunction(id: number): WASMFunction | undefined {
-    if (id >= this.imports.length) {
-      return this.functions.find((f) => {
+    if (id >= this.wasm.imports.length) {
+      return this.wasm.functions.find((f) => {
         return f.id === id;
       });
     } else {
-      return this.imports.find((f) => {
+      return this.wasm.imports.find((f) => {
         return f.id === id;
       });
     }
   }
 
-  public getEnvironmentFunctions(): WASMFunction[] {
-    return this.imports;
-  }
-
-  public getGlobalFromIndex(index: number): VariableInfo | undefined {
-    return this.globals.find((gb) => gb.index === index);
-  }
-
-  public mappings(): SourceCodeMapping[] {
-    let opcodes: SourceCodeMapping[] = [];
-    for (const func of this.functions) {
-      opcodes = opcodes.concat(func.mappings);
-    }
-    return opcodes;
-  }
-
-  public getOpcode(address: number): WasmOpcode | undefined {
-    for (const fun of this.functions) {
-      const opcode = fun.getOpcode(address);
-      if (opcode !== undefined) {
-        return opcode;
+  private createWAT2WASMMappings(
+    infoLines: LineInfoPairs[],
+  ): Map<number, SourceCodeMapping[]> {
+    const map = new Map<number, SourceCodeMapping[]>();
+    for (let i = 0; i < infoLines.length; i++) {
+      if (map.has(infoLines[i].lineInfo.line)) {
+        continue;
       }
+
+      const lines: LineInfoPairs[] = infoLines.filter(
+        (l) => l.lineInfo.line === infoLines[i].lineInfo.line,
+      );
+
+      const mappings: SourceCodeMapping[] = [];
+      for (let j = 0; j < lines.length; j++) {
+        const l = lines[j];
+        const inst = this.wasm.instructionFromAddress(l.lineAddress);
+        if (inst !== undefined) {
+          mappings.push({
+            source: this._sourceCodePath,
+            address: l.lineAddress,
+            linenr: l.lineInfo.line,
+            columnStart: l.lineInfo.columnStart,
+            columnEnd: l.lineInfo.columnEnd,
+            opcode: inst,
+          });
+        }
+      }
+
+      map.set(infoLines[i].lineInfo.line, mappings);
     }
-    return undefined;
+    return map;
   }
 
-  public getPrevSourceCodeMappingFromAddress(
-    wasmAddr: number,
-  ): SourceCodeMapping | undefined {
-    const func = this.functions.find((func: WASMFunction) => {
-      return func.getOpcode(wasmAddr);
-    });
-
-    if (func === undefined) {
-      return undefined;
-    }
-
-    const locations = func.getSourceCodeLocations();
-    let idxPrevLocation = -1;
-    for (let index = 0; index < locations.length; index++) {
-      const location = locations[index];
-      if (location.address === wasmAddr) {
-        idxPrevLocation = index - 1;
-        break;
-      }
-    }
-
-    if (idxPrevLocation < 0) {
-      return undefined;
-    }
-    return locations[idxPrevLocation];
+  private createArrayFromMappings(
+    mappings: Map<number, SourceCodeMapping[]>,
+  ): SourceCodeMapping[] {
+    return Array.from(mappings.values())
+      .flat()
+      .sort((a, b) => {
+        return a.columnStart - b.columnStart;
+      });
   }
 }
