1ea77dcb3ba6e28bf4dc756df62397fbc87cd6e3
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-10 14:37:28 +0200

    add predicate funct

--- src/source_mappers/compilers/prog_language_selection.ts
+++ src/source_mappers/compilers/prog_language_selection.ts
@@ -3,6 +3,10 @@ export enum TargetLanguage {
   AssemblyScript = 'ts',
 }
 
+export function isTargetLanguage(value: any): value is TargetLanguage {
+  return Object.values(TargetLanguage).includes(value);
+}
+
 export interface ProgLangSelectionArgs {
   targetLanguage: TargetLanguage;
   compilerArgs?: any;
