3f1a71249ef9f0216f6d35b49e9787fcd7fb26ff
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-12 01:29:24 +0200

    update sourcemap

--- src/source_mappers/source_map.ts
+++ src/source_mappers/source_map.ts
@@ -3,14 +3,18 @@ import { SourceMapConsumer, type MappingItem } from 'source-map';
 import { type SourceCodeLocation } from './old_source_map';
 import { createLogger } from '../logger/logger';
 import { isAbsolutePath, isFilePath, pathJoin } from '../util/file_util';
+import { WasmModule } from '../webassembly/wasm/wasm_module';
+import { type WASMFunction } from '../webassembly/wasm/wasm_function';
 
 const logger = createLogger('SourceMap');
 
 export class SourceMap {
   private readonly _sourceToAbsPathSource: Map<string, string>;
+  private readonly _sources: string[];
   private readonly _mappings: MappingItem[];
   private readonly _pathToSourceMap: string;
   private readonly _wasmPath: string;
+  public readonly wasm: WasmModule;
 
   constructor(
     pathToSourceMap: string,
@@ -19,9 +23,10 @@ export class SourceMap {
     sources: string[],
     mappings: MappingItem[],
   ) {
-    this._wasmPath = wasmPath;
     this._pathToSourceMap = pathToSourceMap;
+    this._wasmPath = wasmPath;
     this._sourceToAbsPathSource = new Map();
+    this._sources = sources;
     this._mappings = mappings;
 
     if (sources.length !== absolutePathSources.length) {
@@ -33,6 +38,12 @@ export class SourceMap {
     for (let i = 0; i < sources.length; i++) {
       this._sourceToAbsPathSource.set(sources[i], absolutePathSources[i]);
     }
+
+    this.wasm = new WasmModule(this._wasmPath);
+  }
+
+  get sources(): string[] {
+    return this._sources;
   }
 
   public generatedPositionFor(location: SourceCodeLocation): MappingItem[] {
@@ -61,6 +72,68 @@ export class SourceMap {
     return positions;
   }
 
+  // move next method to AST
+  public getFunction(id: number): WASMFunction | undefined {
+    if (id >= this.wasm.imports.length) {
+      return this.wasm.functions.find((f) => {
+        return f.id === id;
+      });
+    } else {
+      return this.wasm.imports.find((f) => {
+        return f.id === id;
+      });
+    }
+  }
+
+  // AS generatedPositionFor
+  // public generatedPositionFor(
+  //   location: SourceCodeLocation,
+  // ): SourceCodeMapping[] {
+  //   const positions: SourceCodeMapping[] = [];
+  //   const candidates = this._mappings.filter((m) => {
+  //     return m.originalLine === location.linenr;
+  //   });
+
+  //   for (const s of candidates) {
+  //     // if (s.originalLine !== location.linenr) {
+  //     //   continue;
+  //     // }
+  //     const colStart = location.columnStart;
+  //     if (colStart !== undefined) {
+  //       if (colStart > s.originalColumn) {
+  //         continue;
+  //       }
+  //     }
+
+  //     const colEnd = location.columnEnd;
+  //     if (colEnd !== undefined) {
+  //       logger.warn(
+  //         `Ignoring columEnd ${colEnd} as sourceMap has only column field`,
+  //       );
+  //     }
+
+  //     const instruction = this.wasm.instructionFromAddress(s.generatedColumn);
+  //     if (instruction === undefined) {
+  //       logger.error(
+  //         `Skipping a wasm instruction unncesserily. TODO remove wasm instruction from mapping`,
+  //       );
+  //       throw new Error(
+  //         `Skipping a wasm instruction unncesserily. TODO remove wasm instruction from mapping`,
+  //       );
+  //     }
+  //     const m: SourceCodeMapping = {
+  //       source: s.source,
+  //       address: s.generatedColumn,
+  //       linenr: location.linenr,
+  //       columnStart: s.originalColumn,
+  //       columnEnd: s.originalColumn,
+  //       instruction,
+  //     };
+  //     positions.push(m);
+  //   }
+  //   return positions;
+  // }
+
   public getOriginalPositionFor(wasmAddr: number): MappingItem | undefined {
     const maps = this._mappings
       .filter((m: MappingItem) => {
@@ -85,6 +158,45 @@ export class SourceMap {
       return maps[0];
     }
   }
+
+  // AS getOriginalPositionFor
+  // public override getOriginalPositionFor(
+  //   wasmAddr: number,
+  // ): SourceCodeMapping | undefined {
+  //   let m = this._mappings.find((m: MappingItem) => {
+  //     return m.generatedColumn === wasmAddr;
+  //   });
+
+  //   let instruction: WasmInstruction | undefined;
+  //   if (m === undefined) {
+  //     instruction = this.wasm.instructionFromAddress(wasmAddr);
+  //     if (instruction === undefined) {
+  //       return undefined;
+  //     }
+  //     m = this._mappings.find((mi: MappingItem) => {
+  //       return mi.generatedColumn === instruction?.startAddress;
+  //     });
+  //   }
+  //   if (m?.source === undefined || m?.source === null) {
+  //     return undefined;
+  //   }
+  //   const src = this._sourceToAbsPathSource.get(m.source);
+  //   if (src === undefined) {
+  //     throw new Error(`No absolutePath set for Source ${m.source}`);
+  //   }
+  //   instruction = this.wasm.instructionFromAddress(wasmAddr);
+  //   if (instruction === undefined) {
+  //     return undefined;
+  //   }
+  //   return {
+  //     source: src,
+  //     address: m.generatedColumn,
+  //     linenr: m.originalLine,
+  //     columnStart: m.originalColumn,
+  //     columnEnd: m.originalColumn,
+  //     instruction,
+  //   };
+  // }
 
   static async fromSourceMapPath(
     pathToRootSource: string,
