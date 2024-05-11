da6543887c448f82fba4ca2bfd72ce509a8b584e
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-11 19:48:10 +0200

    add ts for running wasm-tools metadata command

---
+++ src/language_adaptors/dwarf/metadata_wasm.ts
@@ -0,0 +1,71 @@
+import { spawn } from 'child_process';
+
+export async function readLanguageMetadata(
+  wasmFilePath: string,
+): Promise<string | undefined> {
+  return readMetadataWasm(wasmFilePath, 'language');
+}
+
+async function readMetadataWasm(
+  wasmFilePath: string,
+  metaDataOfInterest: string,
+): Promise<string | undefined> {
+  /*  This command produces a string of the following form.
+   * metaDataOfInterest should be either 'language' or 'processed-by'
+   * example command output:
+   * 'module:
+   *   language:
+   *       Rust
+   *   processed-by:
+   *       rustc: 1.76.0 (07dca489a 2024-02-04)'
+   */
+
+  const metadataKey = metaDataOfInterest.endsWith(':')
+    ? metaDataOfInterest
+    : `${metaDataOfInterest}:`;
+
+  const [exitCode, stdout, stderr] = await runShowMetadataCommand(wasmFilePath);
+  if (exitCode !== 0 || stderr !== '') {
+    console.error(
+      `wasm-tools metadata show failed with code ${exitCode} error ${stderr}`,
+    );
+    return undefined;
+  }
+  const metadata = stdout.split('\n').map((s) => s.trim());
+  const idx = metadata.map((s) => s.toLowerCase()).indexOf(metadataKey);
+
+  if (idx === -1 || idx + 1 >= metadata.length) {
+    throw new Error(`no metadata item found on wasm for ${metaDataOfInterest}`);
+  }
+  return metadata[idx + 1];
+}
+
+async function runShowMetadataCommand(
+  wasmFilePath: string,
+): Promise<[number, string, string]> {
+  return new Promise<[number, string, string]>((resolve, reject) => {
+    const command = ['wasm-tools', 'metadata', 'show', wasmFilePath];
+
+    const process = spawn(command[0], command.slice(1), { stdio: 'pipe' });
+    let stdout = '';
+    let stderr = '';
+
+    process.stdout.on('data', (data) => {
+      stdout += data.toString();
+    });
+
+    process.stderr.on('data', (data) => {
+      stderr += data.toString();
+    });
+
+    process.on('close', (code) => {
+      if (typeof code !== 'number') {
+        throw new Error(`Got a non number exitCode for addr2line`);
+      }
+      resolve([code, stdout, stderr]);
+    });
+    process.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
