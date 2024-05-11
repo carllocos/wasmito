783ffcd71b088590306975cebdbc6b197a0deae0
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-11 23:03:21 +0200

    move folder

--- src/webassembly/parsers/dissambled.ts
+++ src/webassembly/parsers/dissambled.ts
@@ -1,9 +1,6 @@
 import * as fs from 'fs';
-import { WasmInstruction } from '../../webassembly/wasm/wasm_instruction';
-import {
-  WasmOpcodeHasImmediate,
-  wasmOpcodeFromNr,
-} from '../../webassembly/wasm/wasm_opcode';
+import { WasmInstruction } from '../wasm/wasm_instruction';
+import { WasmOpcodeHasImmediate, wasmOpcodeFromNr } from '../wasm/wasm_opcode';
 
 export interface ParsedInstruction {
   address: number;
