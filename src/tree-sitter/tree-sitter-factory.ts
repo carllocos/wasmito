eed46fc1ba8acef06cacad43dc1b31a1e01ffc7f
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-14 21:19:20 +0200

    tmp code to fix failling github action

--- src/tree-sitter/tree-sitter-factory.ts
+++ src/tree-sitter/tree-sitter-factory.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import type Parser from 'web-tree-sitter';
 import { isFilePath, pathJoin } from '../util/file_util';
 import { createLanguageParser } from './tree-sitter-parser';
@@ -7,12 +8,25 @@ function buildWasmParserName(targetLanguage: string): string {
   return pathJoin(d, `./parsers/tree-sitter-${targetLanguage}.wasm`);
 }
 
+function generateAvailableFiles(): string {
+  const p = pathJoin(__dirname, `./parsers/`);
+  const files = fs.readdirSync(p);
+  const filesStr = files
+    .map((f) => {
+      return pathJoin(p, f);
+    })
+    .join(', ');
+
+  return `[${filesStr}]`;
+}
+
 export async function buildASTParser(targetLanguage: string): Promise<Parser> {
   const tl = targetLanguage.toLowerCase();
   const pathToLanguageParser = buildWasmParserName(tl);
   if (!isFilePath(pathToLanguageParser)) {
+    const avFiles = generateAvailableFiles();
     throw new Error(
-      `No AST parser found for language ${tl}. Path to parser ${pathToLanguageParser}`,
+      `No AST parser found for language ${tl}. Path to parser ${pathToLanguageParser}. Available files are ${avFiles}`,
     );
   }
   return createLanguageParser(pathToLanguageParser);
