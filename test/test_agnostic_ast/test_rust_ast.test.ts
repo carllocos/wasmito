bebea3daf6e8793679b9c48f852f8eb352d08268
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-16 08:53:01 +0200

    add test cases for most searching most specialised node

--- test/test_agnostic_ast/test_rust_ast.test.ts
+++ test/test_agnostic_ast/test_rust_ast.test.ts
@@ -3,6 +3,10 @@ import { expect } from 'chai';
 import path from 'path';
 import { pathJoin } from '../../src/util/file_util';
 import { AgnosticAST } from '../../src/ast/angostic-ast';
+import {
+  mostSpecialisedNode,
+  sourceLocationToNodePosition as srcLocToASTPos,
+} from '../../src/tree-sitter/tree-sitter-parser';
 
 const rustExamplesPath = path.resolve('./test/data/rust_examples/');
 
@@ -25,5 +29,32 @@ describe('Rust Blink App AST Building and Operations on AST', () => {
     } catch (e) {
       assert.fail(`Test failed reason: ${e}`);
     }
+  });
+
+  it('MostSpecialisedNode of unexisting source location should yield undefined', () => {
+    const noValidPos = srcLocToASTPos(45, 1);
+    const foundNode = mostSpecialisedNode(blinkAST.ast, noValidPos);
+    expect(foundNode).to.equal(undefined);
+  });
+
+  it('MostSpecialidNode on Source loc (45,5) should be the "const" of the declaration', () => {
+    const ledDeclPos = srcLocToASTPos(45, 5);
+    const n = mostSpecialisedNode(blinkAST.ast, ledDeclPos);
+    expect(n).to.not.equal(undefined);
+    expect(n?.text).to.equal('const');
+  });
+
+  it('MostSpecialisedNode on Source loc (45,22) should be the assigned "10" to the LED const', () => {
+    const valueAssignedToLedDecl = srcLocToASTPos(45, 22);
+    const n = mostSpecialisedNode(blinkAST.ast, valueAssignedToLedDecl);
+    expect(n).to.not.equal(undefined);
+    expect(n?.text).to.equal('10');
+  });
+
+  it('MostSpecialisedNode on Source loc (55,31) should be the "old_delta" parameter of the lambda', () => {
+    const lambdaParameterLoc = srcLocToASTPos(55, 31);
+    const n = mostSpecialisedNode(blinkAST.ast, lambdaParameterLoc);
+    expect(n).to.not.equal(undefined);
+    expect(n?.text).to.equal('old_delta');
   });
 });
