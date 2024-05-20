9f50ea53b8d3648bddd0d547f0bb9458f3f9c14e
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-20 19:46:44 +0200

    add dot serialization file

--- src/cfg/index.ts
+++ src/cfg/index.ts
@@ -1,1 +1,2 @@
+export * from './serialize_wasm_cfg';
 export * from './wasm_cfg';
---
+++ src/cfg/serialize_wasm_cfg.ts
@@ -0,0 +1,60 @@
+import { getEdgeNodes, getNode, type CFGNode, type Graph } from './wasm_cfg';
+
+export function controlFlowGraphToDot(g: Graph, nameGraph: string): string {
+  const header = `digraph "CFG of ${nameGraph}" `;
+  const nodesDone = new Set<number>();
+  let nodesStr = '';
+  const nodes: CFGNode[] = [];
+  for (const [addr, node] of g.entries()) {
+    if (nodesDone.has(node.nodeID)) {
+      continue;
+    }
+    const n = getNode(g, addr);
+    const record = n.instructions.length > 1 ? 'Mrecord' : 'record';
+
+    const instructionsStrs: string[] = [];
+    for (let i = 0; i < n.instructions.length; i++) {
+      const instr = n.instructions[i];
+      const instrIdx = n.instructionsIndexes[i];
+
+      instructionsStrs.push(
+        `instr ${instrIdx}: (start ${instr.startAddress}, end ${instr.endAddress}) ${instr.name} ${instr.immediate ?? ''} ${instr.args}`,
+      );
+    }
+    let s = '';
+    if (instructionsStrs.length === 1) {
+      s = instructionsStrs[0];
+    } else {
+      s = instructionsStrs
+        .map((instStr) => {
+          return `{${instStr}\\l}`;
+        })
+        .join('|');
+    }
+
+    const label = `{${n.changesFlow ? 'Control' : 'Data'} block ${n.nodeID}|${s}}`;
+
+    const nodeStr = `block${n.nodeID} [shape=${record}, label="${label}"];\n`;
+    nodesStr += nodeStr;
+    nodesDone.add(n.nodeID);
+    nodes.push(n);
+  }
+
+  const edgesStr: string[] = [];
+  for (const n of nodes) {
+    const nodeId = `block${n.nodeID}`;
+    const edges = n.edges;
+    const str = getEdgeNodes(g, n)
+      .map((edgeNode, i) => {
+        const e = edges[i];
+        const instFrom = e.instrFrom;
+        const instTo = e.instrTo;
+        return `${nodeId} -> block${edgeNode.nodeID} [label="from ${instFrom.startAddress} to ${instTo.startAddress}"];\n`;
+      })
+      .join('');
+    edgesStr.push(str);
+  }
+  const allEdges = edgesStr.join('');
+
+  return `${header}{\n${nodesStr}${allEdges}}`;
+}
