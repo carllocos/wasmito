21eada2efc0ab13f0ce95951184554bfff160f14
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-22 11:43:44 +0200

    build source cfg + add initial tests for source cfg

---
+++ test/test_language_adaptors/test_ast_cfg.test.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import path from 'path';
+import { SourceMapfromDWARFWasm } from '../../src/source_mappers/source_map_builder';
+import { constructLanguageAdaptor } from '../../src/language_adaptors/language_adaptor';
+import assert, { fail } from 'assert';
+import { type ASTControlFlowGraph } from '../../src/language_adaptors/ast_cfg';
+
+const exampleFile = path.resolve('./test/data/rust_examples/blink/main.wasm');
+describe('Rust AST Control Flow Graph for Blink App', function () {
+  let astCFG: ASTControlFlowGraph;
+
+  before('parse wasm module', async function () {
+    try {
+      const sm = await SourceMapfromDWARFWasm(exampleFile);
+      const langAdaptor = await constructLanguageAdaptor(sm);
+      assert(langAdaptor.astCFG !== undefined);
+      astCFG = langAdaptor.astCFG;
+    } catch (e) {
+      fail(`Could not construct sourcemap or langadaptor. Reason ${e}`);
+    }
+  });
+
+  it('build cfg function with id 7', function () {
+    const startWasmAddr = 493; // Source Loc (44, 1)
+    const nodes = astCFG.nodesFromAddress(startWasmAddr);
+    expect(nodes.length).to.greaterThan(0);
+  });
+});
