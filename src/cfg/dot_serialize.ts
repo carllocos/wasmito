291afe19dd13f645e70f2048533297cef5b62dbf
carllocos <23368410+carllocos@users.noreply.github.com>
2024-05-22 13:35:41 +0200

    add serialisation of source cfg to dot

--- src/cfg/dot_serialize.ts
+++ src/cfg/dot_serialize.ts
@@ -1,3 +1,8 @@
+import { isCallIndirect, isCallInstruction } from '../webassembly';
+import {
+  sourceCFGHasOutgoingFunCallEdges,
+  type SourceCFGNode,
+} from './source_cfg';
 import {
   getWasmNodeEdges,
   getWasmCFGNode,
@@ -61,6 +66,96 @@ export function wasmControlFlowGraphToDot(
       })
       .join('');
     edgesStr.push(str);
+  }
+  const allEdges = edgesStr.join('');
+
+  return `${header}{\n${nodesStr}${allEdges}}`;
+}
+
+export function sourceControlFlowGraphToDot(
+  nodes: SourceCFGNode[], // multiple nodes as there may be multiple entry nodes per function
+  nameGraph: string,
+): string {
+  const header = `digraph "CFG of ${nameGraph}" `;
+  const nodesDone = new Set<number>();
+  let nodesStr = '';
+  const exitNodesToAdd = new Set<number>();
+  for (const n of nodes) {
+    if (nodesDone.has(n.nodeId)) {
+      continue;
+    }
+    if (sourceCFGHasOutgoingFunCallEdges(n)) {
+      for (const callinstr of n.edgesToOutSideCalls) {
+        if (isCallInstruction(callinstr)) {
+          exitNodesToAdd.add(callinstr.funIdx);
+        } else if (isCallIndirect(callinstr)) {
+          throw new Error(`Call indirect not yet supported`);
+        } else {
+          throw new Error(`outgoing instructions should be (indirect) calls`);
+        }
+      }
+    }
+    const record = n.instructions.length > 1 ? 'Mrecord' : 'record';
+
+    const instructionsStrs: string[] = [];
+    for (let i = 0; i < n.instructions.length; i++) {
+      const instr = n.instructions[i];
+      const instrIdx = n.instructionsIndexes[i];
+
+      instructionsStrs.push(
+        `instr ${instrIdx}: (start ${instr.startAddress}, end ${instr.endAddress}) ${instr.name} ${instr.immediate ?? ''} ${instr.args}`,
+      );
+    }
+    let s = '';
+    if (instructionsStrs.length === 1) {
+      s = instructionsStrs[0];
+    } else {
+      s = instructionsStrs
+        .map((instStr) => {
+          return `{${instStr}\\l}`;
+        })
+        .join('|');
+    }
+
+    const label = `{Data block ${n.nodeId}|${s}}`;
+
+    const nodeStr = `block${n.nodeId} [shape=${record}, label="${label}"];\n`;
+    nodesStr += nodeStr;
+    nodesDone.add(n.nodeId);
+    nodes.push(n);
+  }
+
+  for (const fid of exitNodesToAdd.values()) {
+    const record = 'record';
+    const label = `Data block ${fid}|instr<4>call ${fid}}`;
+    nodesStr += `block${fid} [shape=${record}, label="${label}"];\n`;
+  }
+
+  const alreadyVisitedNodes = new Set<number>();
+  const edgesStr: string[] = [];
+  for (const n of nodes) {
+    if (alreadyVisitedNodes.has(n.nodeId)) {
+      continue;
+    }
+    alreadyVisitedNodes.add(n.nodeId);
+    const nodeId = `block${n.nodeId}`;
+    const str = n.edges
+      .map((edgeNode) => {
+        return `${nodeId} -> block${edgeNode.nodeId};\n`;
+      })
+      .join('');
+    edgesStr.push(str);
+    if (sourceCFGHasOutgoingFunCallEdges(n)) {
+      for (const callInstr of n.edgesToOutSideCalls) {
+        if (isCallInstruction(callInstr)) {
+          edgesStr.push(`${nodeId} -> block${callInstr.funIdx};\n`);
+        } else if (isCallIndirect(callInstr)) {
+          throw new Error(`Call indirect not yet supported`);
+        } else {
+          throw new Error(`outgoing instructions should be (indirect) calls`);
+        }
+      }
+    }
   }
   const allEdges = edgesStr.join('');
 
