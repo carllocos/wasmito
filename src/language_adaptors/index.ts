9e4dfb7bd0a369774814463d10d7a1b4454c397d
carllocos <23368410+carllocos@users.noreply.github.com>
2024-06-20 14:44:06 +0200

    export lang config sourc efiles

--- src/language_adaptors/index.ts
+++ src/language_adaptors/index.ts
@@ -1,3 +1,5 @@
+export * from './languages/language_config';
+export * from './languages/all_langs';
 export * from './agnostic_node';
 export * from './debug_tree_operations';
 export * from './language_adaptor';
