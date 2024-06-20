75f71bf09180069a2b6ebd377cb7f8697acb2bd2
carllocos <23368410+carllocos@users.noreply.github.com>
2024-06-20 14:41:19 +0200

    build parser via parser name

--- src/tree-sitter/tree-sitter-factory.ts
+++ src/tree-sitter/tree-sitter-factory.ts
@@ -1,41 +1,18 @@
-import * as fs from 'fs';
 import type Parser from 'web-tree-sitter';
 import { isFilePath, pathJoin } from '../util/file_util';
 import { createLanguageParser } from './tree-sitter-parser';
 
-function buildWasmParserName(targetLanguage: string): string {
+function buildWasmParserName(parserName: string): string {
   const d = __dirname;
-  return pathJoin(d, `./parsers/tree-sitter-${targetLanguage}.wasm`);
+  return pathJoin(d, `./parsers/${parserName}`);
 }
 
-function generateAvailableFiles(): string {
-  const p = pathJoin(__dirname, `./parsers/`);
-  const files = fs.readdirSync(p);
-  const filesStr = files
-    .map((f) => {
-      return pathJoin(p, f);
-    })
-    .join(', ');
-
-  return `[${filesStr}]`;
-}
-
-export async function buildASTParser(targetLanguage: string): Promise<Parser> {
-  const tl = targetLanguage.toLowerCase();
-  const pathToLanguageParser = buildWasmParserName(tl);
+export async function buildASTParser(parserName: string): Promise<Parser> {
+  const pathToLanguageParser = buildWasmParserName(parserName);
   if (!isFilePath(pathToLanguageParser)) {
-    const avFiles = generateAvailableFiles();
     throw new Error(
-      `No AST parser found for language ${tl}. Path to parser ${pathToLanguageParser}. Available files are ${avFiles}`,
+      `No AST parser found with name ${parserName}. Path to parsers ${pathToLanguageParser}`,
     );
   }
   return createLanguageParser(pathToLanguageParser);
-}
-
-export async function createTypeScriptParser(): Promise<Parser> {
-  return buildASTParser('typescript');
-}
-
-export async function createRustParser(): Promise<Parser> {
-  return buildASTParser('rust');
 }
