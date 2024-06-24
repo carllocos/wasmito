83f37d0e75d813e96e75f346a84d65cfe9d8b7eb
carllocos <23368410+carllocos@users.noreply.github.com>
2024-06-24 21:51:45 +0200

    fix tests of parsers

--- test/test_agnostic_ast/tree-parser.test.ts
+++ test/test_agnostic_ast/tree-parser.test.ts
@@ -1,11 +1,13 @@
 import { expect } from 'chai';
 import { buildASTParser } from '../../src/tree-sitter/tree-sitter-factory';
 import assert from 'assert';
+import { RustLangConfig } from '../../src/language_adaptors/languages/rust_config';
+import { AssemblyScriptLangConfig } from '../../src/language_adaptors/languages/assemblyscript_config';
 
 describe('Building Language Parsers', () => {
   it('Construct Parser for Rust', async () => {
     try {
-      const parser = await buildASTParser('rust');
+      const parser = await buildASTParser(RustLangConfig.parserPath);
       expect(parser).to.not.equal(undefined);
     } catch (e) {
       assert.fail(`Could not construct parser for Rust. ${e}`);
@@ -14,7 +16,7 @@ describe('Building Language Parsers', () => {
 
   it('Construct Parser for TypeScript', async () => {
     try {
-      const parser = await buildASTParser('typescript');
+      const parser = await buildASTParser(AssemblyScriptLangConfig.parserPath);
       expect(parser).to.not.equal(undefined);
     } catch (e) {
       assert.fail(`Could not construct parser for Typescript. ${e}`);
