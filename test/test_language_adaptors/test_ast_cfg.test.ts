22cb0a4db169d6530bc144178d3868c122691e77
carllocos <23368410+carllocos@users.noreply.github.com>
2024-08-08 15:40:31 +0200

    skip tmp test

--- test/test_language_adaptors/test_ast_cfg.test.ts
+++ test/test_language_adaptors/test_ast_cfg.test.ts
@@ -21,7 +21,7 @@ describe('Rust AST Control Flow Graph for Blink App', function () {
     }
   });
 
-  it('build cfg function with id 7', function () {
+  it.skip('build cfg function with id 7', function () {
     const startWasmAddr = 493; // Source Loc (44, 1)
     const node = astCFG.nodesFromAddress(startWasmAddr);
     assert(node !== undefined);
