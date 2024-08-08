168013dd946aeb0f8b4e0e562f8b32852bd34f2b
carllocos <23368410+carllocos@users.noreply.github.com>
2024-08-08 16:39:04 +0200

    use path.resolve and catch error if not valid filepath provided

--- src/tree-sitter/tree-sitter-factory.ts
+++ src/tree-sitter/tree-sitter-factory.ts
@@ -1,9 +1,10 @@
+import * as path from 'path';
 import type Parser from 'web-tree-sitter';
 import { isFilePath, listFilesInDirectory, pathJoin } from '../util/file_util';
 import { createLanguageParser } from './tree-sitter-parser';
 
 function parsersRootPath(): string {
-  const d = __dirname;
+  const d = path.resolve(__dirname);
   return pathJoin(d, `./parsers/`);
 }
 
@@ -15,8 +16,13 @@ function buildWasmParserName(parserName: string): string {
 export async function buildASTParser(parserName: string): Promise<Parser> {
   const pathToLanguageParser = buildWasmParserName(parserName);
   if (!isFilePath(pathToLanguageParser)) {
-    const files = await listFilesInDirectory(parsersRootPath());
-    const filesStr = files.join(', ');
+    let filesStr = 'failed to find';
+    try {
+      const files = await listFilesInDirectory(parsersRootPath());
+      filesStr = files.join(', ');
+    } catch (er) {
+      filesStr = `failed to find with error ${er}`;
+    }
     throw new Error(
       `No AST parser found with name ${parserName}. Path to parsers ${pathToLanguageParser}. Files found there: ${filesStr}`,
     );
