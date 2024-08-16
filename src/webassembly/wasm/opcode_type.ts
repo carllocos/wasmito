c87b1a0118829276a0f07af92eda27fd713d3fc7
carllocos <23368410+carllocos@users.noreply.github.com>
2024-08-16 18:54:25 +0200

    add fields to keep track of the actual types

--- src/webassembly/wasm/opcode_type.ts
+++ src/webassembly/wasm/opcode_type.ts
@@ -1,12 +1,34 @@
+import { type WASM } from '../wasm';
+
 export class WasmType {
   public readonly nrArgs: number;
+  private _args: WASM.Type[];
   public readonly nrResults: number;
+  private _returnTypes: WASM.Type[];
   public readonly id?: number;
 
   constructor(nrArgs: number, nrResults: number, id?: number) {
     this.nrArgs = nrArgs;
     this.nrResults = nrResults;
     this.id = id;
+    this._args = [];
+    this._returnTypes = [];
+  }
+
+  get args(): WASM.Type[] {
+    return this._args;
+  }
+
+  set args(a: WASM.Type[]) {
+    this._args = a;
+  }
+
+  get returnTypes(): WASM.Type[] {
+    return this._returnTypes;
+  }
+
+  set returnTypes(a: WASM.Type[]) {
+    this._returnTypes = a;
   }
 
   public hasResult(): boolean {
