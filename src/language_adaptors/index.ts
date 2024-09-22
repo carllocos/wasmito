d8c458d6d2552511c8f5f9e446491cb6cad5b382
carllocos <23368410+carllocos@users.noreply.github.com>
2024-09-22 09:47:58 +0200

    update index

--- src/language_adaptors/index.ts
+++ src/language_adaptors/index.ts
@@ -1,5 +1,5 @@
-export * from './languages/language_config';
-export * from './languages/all_langs';
+export * from './language_config';
+export * from './all_langs';
 export * from './agnostic_node';
 export * from './debug_tree_operations';
 export * from './language_adaptor';
