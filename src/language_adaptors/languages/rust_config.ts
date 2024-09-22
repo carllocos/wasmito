8d09bb95bb87bced84d91e5cc8a2e87a809f3047
carllocos <23368410+carllocos@users.noreply.github.com>
2024-09-22 09:40:58 +0200

    rmv lang config in favor of json

--- src/language_adaptors/languages/rust_config.ts
+++
@@ -1,8 +0,0 @@
-import { type LanguageConfiguration } from './language_config';
-
-export const RustLangConfig: LanguageConfiguration = {
-  language: 'rust',
-  fileExtensions: ['rs'],
-  parserPath: 'tree-sitter-rust.wasm',
-  astDebugOperations: [],
-};
