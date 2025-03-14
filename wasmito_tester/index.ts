039f73f0f27f132ee295d20910252bff8f79c287
carllocos <23368410+carllocos@users.noreply.github.com>
2025-03-14 12:59:25 +0100

    add index for tester

---
+++ wasmito_tester/index.ts
@@ -0,0 +1,5 @@
+export * from './reausable_system_setups';
+export * from './reusable_actions';
+export * from './shared_interfaces';
+export * from './system_deployer';
+export * from './system_tester';
