853560701ee24ec8d0140229aa1220e2407d6d09
carllocos <23368410+carllocos@users.noreply.github.com>
2025-03-31 16:02:53 +0000

    finalise dockerfile + [WIP] install script for bench debug ops

---
+++ install_bench_debug_ops.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+# The following is specific to the benchmarks
+
+ARDUINO_DIR="$1"
+if [ -z $ARDUINO_DIR ]; then
+  echo "ARDUINO_DIR is missing"
+  exit 1
+fi
+
+if [ ! -d "$ARDUINO_DIR" ]; then
+  echo "ARDUINO_DIR is not a directory"
+  exit 1
+fi
+ARDUINO_DIR=$(realpath $ARDUINO_DIR)
+
+echo ARDUINO_DIR $ARDUINO_DIR
+
+ARDUINO_CLI="$ARDUINO_DIR/arduino-cli"
+ARDUINO_CONFIG="$ARDUINO_DIR/arduino_config.yml"
+
+echo Installing m5stack core platforms....
+$ARDUINO_CLI core install m5stack:esp32@2.0.0 --config-file $ARDUINO_CONFIG
+
+echo Compiling FlappyBird as example
+cp -r $ARDUINO_DIR/libraries/M5StickC/examples/Games/FlappyBird/ . 
+cd ./FlappyBird/          
+$ARDUINO_CLI compile --fqbn m5stack:esp32:m5stick-c --config-file $ARDUINO_CONFIG --verbose -e
