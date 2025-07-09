233f80425978419b6946b55388455ac644fa96ea
carllocos <23368410+carllocos@users.noreply.github.com>
2025-07-09 11:26:49 +0200

    move creation of project config in seperate bash

--- install_all.sh
+++ install_all.sh
@@ -61,28 +61,8 @@ make wat2wasm
 echo building wasm-objdump
 make wasm-objdump
 
-echo "> Wasmito: Creating libs config"
+# build project config file
 cd $LIBS_DIR/../ # go to root of project
-WASMITO_DIR=.wasmito
-if [ ! -d "$WASMITO_DIR" ]; then
-    mkdir "$WASMITO_DIR"
-    echo "Directory '$WASMITO_DIR' created."
-else
-    echo "Directory '$WASMITO_DIR' already exists."
-fi
-
-SDK_CONFIG_FILE=".wasmito/sdk_config.cfg"
-if [ ! -f "$SDK_CONFIG_FILE" ]; then
-    touch $SDK_CONFIG_FILE
-    echo "File '$SDK_CONFIG_FILE' created."
-else
-    echo "File '$SDK_CONFIG_FILE' already exists."
-fi
-
-echo "WARDUINO_SDK=$LIBS_DIR/WARDuino" >> $SDK_CONFIG_FILE
-echo "WABT=$LIBS_DIR/wabt" >> $SDK_CONFIG_FILE
-echo "ARDUINO_CLI=$ARDUINO_CLI" >> $SDK_CONFIG_FILE
-echo "ARDUINO_CONFIG=$ARDUINO_CONFIG" >> $SDK_CONFIG_FILE
-echo "ARDUINO_LIBS=$LIBS_DIR" >> $SDK_CONFIG_FILE
+/bin/sh ./scripts/project_config.sh $LIBS_DIR $ARDUINO_CLI $ARDUINO_CONFIG
 
 npm install && npm run build
