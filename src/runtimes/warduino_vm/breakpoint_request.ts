7dcf1b387231b19e96481e3c8316c6256dbb1eed
carllocos <23368410+carllocos@users.noreply.github.com>
2025-07-24 16:02:06 +0200

    integrate warduino backend

---
+++ src/runtimes/warduino_vm/breakpoint_request.ts
@@ -0,0 +1,56 @@
+import { serializeUInt } from "../../util/encoder";
+import { APIRequestInvalidParse, APIRequestNoSubscription } from "../request_interface";
+
+export class AddBreakpointRequest extends APIRequestNoSubscription<number> {
+  private readonly instructionNr: string;
+  readonly wasmAddr: number;
+
+  constructor(wasmAddr: number) {
+    super();
+    this.wasmAddr = wasmAddr;
+    this.instructionNr = '06';
+  }
+
+  description(): string {
+    return 'AddBreakPointRequest';
+  }
+
+  getData(): string {
+    const encodedAddr = serializeUInt(this.wasmAddr, 4, true);
+    return `${this.instructionNr}${encodedAddr}\n`;
+  }
+
+  parse(data: string): number {
+    if (data === `BP ${this.wasmAddr}!`) {
+      return this.wasmAddr;
+    }
+    throw new APIRequestInvalidParse('No ack for addbp');
+  }
+}
+
+export class RemoveBreakpointRequest extends APIRequestNoSubscription<number> {
+  private readonly instructionNr: string;
+  private readonly wasmAddr: number;
+
+  constructor(wasmAddr: number) {
+    super();
+    this.wasmAddr = wasmAddr;
+    this.instructionNr = '07';
+  }
+
+  description(): string {
+    return 'RemoveBreakPointRequest';
+  }
+
+  getData(): string {
+    const encodedAddr = serializeUInt(this.wasmAddr, 4, true);
+    return `${this.instructionNr}${encodedAddr}\n`;
+  }
+
+  parse(data: string): number {
+    if (data === `BP ${this.wasmAddr}!`) {
+      return this.wasmAddr;
+    }
+    throw new APIRequestInvalidParse('No ack for rmvbp');
+  }
+}
---
+++ src/runtimes/warduino_vm/warduino_vm.ts
@@ -0,0 +1,162 @@
+import { type Channel } from '../../communication/channel_interface';
+import { Command } from '../../communication/command';
+import { type APIRequest } from '../request_interface';
+import { StateRequest } from '../../runtimes/wasmito_vm/requests/inspect_request';
+import { RunRequest } from '../../runtimes/wasmito_vm/requests/run_request';
+import { StepRequest } from '../../runtimes/wasmito_vm/requests/step_request';
+import { WASM, type WasmState } from '../../../src/webassembly/wasm';
+import { AddBreakpointRequest, RemoveBreakpointRequest } from './breakpoint_request';
+import { RuntimeToolAPI } from '../runtime_api';
+import { Breakpoint } from '../../debugger/breakpoint';
+import { SourceCFGNode } from '../../cfg';
+import { BreakpointPolicy } from '../../debugger';
+import { Hook } from '../../hooks';
+import { SourceCodeLocation } from '../../source_mappers';
+import { WASMFunction } from '../../webassembly';
+import { ProxyCallResponse } from '../wasmito_vm/requests/fun_call_request';
+
+export class WARDuinoRuntimeAPI implements RuntimeToolAPI {
+  runtimeName: string;
+  protected channel: Channel;
+  private listenerActivated: boolean;
+  private readonly breakpoints: Set<number>;
+
+  private bpListeners: Array<(bpAddr: number) => void>;
+  private readonly removedListeners: Set<(bpAddr: number) => void>;
+
+  constructor(channel: Channel) {
+    this.channel = channel;
+    this.runtimeName = '';
+    this.listenerActivated = false;
+    this.removedListeners = new Set();
+    this.bpListeners = [];
+    this.breakpoints = new Set();
+  }
+
+  private listenForOnBPReach(data: string): void {
+    const match = data.match(/^AT (\d+)!$/);
+    if (match !== undefined && match !== null) {
+      const bpAddr = parseInt(match[1]);
+      if (isNaN(bpAddr)) {
+        throw new Error(`Could not parse BP address '${match[1]}' to a number`);
+      }
+      this.bpListeners.forEach((lstnr) => {
+        if (!this.removedListeners.has(lstnr)) {
+          lstnr(bpAddr);
+        }
+      });
+
+      this.bpListeners = this.bpListeners.filter((h) => {
+        return !this.removedListeners.has(h);
+      });
+      this.removedListeners.clear();
+    }
+  }
+
+  async addBreakpoint(breakpoint: Breakpoint, timeout?: number): Promise<boolean> {
+    if (!this.listenerActivated) {
+      this.channel.addOnData(this.listenForOnBPReach.bind(this));
+      this.listenerActivated = true;
+    }
+
+    const addr = breakpoint.wasmAddress;
+    const req = new AddBreakpointRequest(addr);
+    const resp = await this.sendRequest(req, timeout);
+    if (resp === addr) {
+      this.breakpoints.add(addr);
+      // if (!this.listenerActivated) {
+      //   this.listenerActivated = true;
+      // }
+      return true;
+    }
+    return false;
+  }
+
+  async removeBreakpoint(breakpoint: Breakpoint, timeout?: number): Promise<boolean> {
+    const addr = breakpoint.wasmAddress;
+    const req = new RemoveBreakpointRequest(addr);
+    const resp = await this.sendRequest(req, timeout);
+    if (resp === addr) {
+      this.breakpoints.delete(addr);
+      // if (this.breakpoints.size === 0) {
+      //   this.channel.removeOnData(this.listenForOnBPReach.bind(this));
+      //   this.listenerActivated = false;
+      // }
+      return true;
+    }
+    return false;
+  }
+
+  onBreakpoint(handler: (bpAdrr: number) => void): void {
+    this.bpListeners.push(handler);
+  }
+
+  removeOnBreakpoint(handler: (bpAdrr: number) => void): void {
+    this.removedListeners.add(handler);
+  }
+
+  async run(timeout?: number): Promise<boolean> {
+    const req = new RunRequest();
+    const resp = await this.sendRequest(req, timeout);
+    return resp === 'GO!';
+  }
+
+  async inspectPC(timeout?: number): Promise<number> {
+    const req = new StateRequest();
+    req.includePC();
+    const resp: WasmState = await this.sendRequest(req, timeout);
+    if (resp.pc === undefined) {
+      throw new Error(`InspectPC failed in to return`);
+    }
+    return resp.pc;
+  }
+
+  async step(timeout?: number): Promise<void> {
+    const req = new StepRequest();
+    const resp = await this.sendRequest(req, timeout);
+    // return resp === 'STEP!';
+    return
+  }
+
+  async startRuntime(timeout: number): Promise<boolean> {
+    this.bpListeners.length = 0;
+    this.removedListeners.clear();
+    this.breakpoints.clear();
+    this.listenerActivated = false;
+    return true;
+  }
+
+  async stopRuntime(timeout: number): Promise<boolean> {
+    throw new Error('stopRuntime is not applicable to this class');
+  }
+
+  private async sendRequest<T>(
+    request: APIRequest<T>,
+    timeout?: number,
+  ): Promise<T> {
+    const command = new Command(this.channel, request, timeout);
+    return command.execute();
+  }
+
+  pause(timeout?: number): Promise<void> { throw new Error("To implement"); }
+  breakpointPolicy(): BreakpointPolicy { throw new Error("To implement"); }
+  changeBreakpointPolicy(p: BreakpointPolicy): void { throw new Error("To implement"); }
+  proxify(timeout?: number): Promise<void> { throw new Error("To implement"); }
+  uploadSourceCode(sourceCodeCompilationArgs: any, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  inspect(neededState: StateRequest, timeout?: number): Promise<WasmState> { throw new Error("To implement"); }
+  snapshot(timeout?: number): Promise<WasmState> { throw new Error("To implement"); }
+  loadWasmState(state: WasmState, timeout?: number): Promise<void> { throw new Error("To implement"); }
+  resolveEvent(timeout?: number): Promise<void> { throw new Error("To implement"); }
+  unregisterFuncForProxyCall(funcToProxy: WASMFunction, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  registerFuncForProxyCall(funcToProxy: WASMFunction, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  functionsProxied(): Set<WASMFunction> { throw new Error("To implement"); }
+  proxyCall(funcid: number, args: WASM.Value[], timeout?: number): Promise<ProxyCallResponse> { throw new Error("To implement"); }
+  addHookBefore(sourceCodeLocation: SourceCodeLocation, hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  addHookAfter(sourceCodeLocation: SourceCodeLocation, hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  addHookBeforeSrcNode(node: SourceCFGNode, hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  addHookOnNewEvent(hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  addHookOnEventHandling(hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  addHookOnError(hook: Hook, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+  subscribeOnNewEvent(cb: (ev: WASM.Event) => void, timeout?: number): Promise<boolean> { throw new Error("To implement"); }
+}
+
