52143b4e869f1f3f3d43b709766a561a6cf929b5
carllocos <23368410+carllocos@users.noreply.github.com>
2025-07-25 17:24:10 +0200

    update lint config to ignore any type

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,36 @@
 // @ts-check
 
-import {globalIgnores } from "eslint/config";
+import { globalIgnores } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import path from "node:path";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import path from 'node:path';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default tseslint.config(
-   globalIgnores([
-    "dist/*",
-    "**/libs/",
-    "**/assemblyscript_examples/",
-    "wasmito_tester/cli.js",
+  globalIgnores([
+    'dist/*',
+    '**/libs/',
+    '**/assemblyscript_examples/',
+    'wasmito_tester/cli.js',
+    'wasmito_tester/test_framework.ts',
   ]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
 
   {
-    extends: compat.extends("prettier"),
+    extends: compat.extends('prettier'),
     plugins: {
       prettier,
     },
@@ -38,22 +39,25 @@ export default tseslint.config(
         ...globals.node,
       },
 
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
     rules: {
-        "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/return-await": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-
-        "prettier/prettier": ["error", {
-            semi: true,
-            singleQuote: true,
-            tabWidth: 2,
-            trailingComma: "all",
-            printWidth: 80,
-        }],
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/return-await': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 80,
+        },
+      ],
     },
   },
 );
