ea32d9664d1022a7c3032cf5ec2231359e3e7bed
carllocos <23368410+carllocos@users.noreply.github.com>
2025-09-25 12:57:59 +0200

    add url from install script

--- scripts/install_arduino_m5stickc.sh
+++ scripts/install_arduino_m5stickc.sh
@@ -5,7 +5,8 @@
 set -e
 
 CLI=$(realpath .)/dist/cjs/cli/cli.cjs
-#TODO add additional url into config then install
-# url: https://m5stack.oss-cn-shenzhen.aliyuncs.com/resource/arduino/package_m5stack_index.json
-# remove also the additional url from template
-node $CLI arduino-cli core install m5stack:esp32@2.0.0
+
+node $CLI arduino-cli config add \
+    board_manager.additional_urls \
+    https://m5stack.oss-cn-shenzhen.aliyuncs.com/resource/arduino/package_m5stack_index.json \
+    && node $CLI arduino-cli core install m5stack:esp32@2.0.0
