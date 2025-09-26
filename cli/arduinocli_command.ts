ee897e8fe54401efec9335beeb16aba2720466a5
carllocos <23368410+carllocos@users.noreply.github.com>
2025-09-26 13:19:29 +0200

    remove redundant method

--- cli/arduinocli_command.ts
+++ cli/arduinocli_command.ts
@@ -1,5 +1,4 @@
 import { type Command } from 'commander';
-import { getPathArduinoCLI, getPathArduinoConfig } from '../src/project_config';
 import { runArduinoCommand } from '../src/platforms/arduino_platform';
 
 export function registerArduinoCLICommand(program: Command): void {
@@ -7,18 +6,8 @@ export function registerArduinoCLICommand(program: Command): void {
     .command('arduino-cli <arguments...>')
     .description('run arduino-cli command on your project')
     .action(async (args) => {
-      const cmdArgs = args.join(' ');
-      const output = await ArduinCommand(cmdArgs);
+      // TODO sanitize arguments
+      const output = await runArduinoCommand(args);
       console.log(output);
     });
-}
-
-async function ArduinCommand(cmdArgs: string): Promise<string> {
-  const arduino_cli = getPathArduinoCLI();
-  const arduino_config = getPathArduinoConfig();
-  let cmd = `${arduino_cli} ${cmdArgs}`;
-  if (arduino_config !== undefined) {
-    cmd = `${cmd} --config-file ${arduino_config}`;
-  }
-  return await runArduinoCommand(cmd);
 }
