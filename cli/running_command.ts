d510df4e1239b1a11924e78b1e8a180dc2b86b93
carllocos <23368410+carllocos@users.noreply.github.com>
2025-10-08 14:26:37 +0200

    CL running command updated
    - Running command can now target WARDuino on the MCU via serial
    - Also possible to monitor the output with `--monitor` flag after changing the running status of the VM
    - description of the command got updated to reflect the latest changes

--- cli/running_command.ts
+++ cli/running_command.ts
@@ -3,18 +3,32 @@ import { ClientSideSocket } from '../src/communication/client_socket';
 import { sendRequest } from '../src/runtimes/request_interface';
 import { PauseRequest } from '../src/runtimes/wasmito_vm/requests/pause_request';
 import { RunRequest } from '../src/runtimes/wasmito_vm/requests/run_request';
+import { isSerialPort } from '../src/util/serial_port';
+import { SerialConnection } from '../src/communication/serial';
 
 export function registerRunningRequestCommand(program: Command): void {
   const commandName = 'running';
   program
     .command(`${commandName} <port>`)
-    .description('manage events on MCU')
+    .description(
+      'Pause or resume execution of a Wasm deployed on a local or MCU VM. For local VM `port` is a socket port. For MCU, `port` is a serial port and -b is required',
+    )
+    .option('-b <baudrate>', 'baudrate only required if port is a serial port')
     .option('--pause', 'pause execution')
     .option('--resume', 'resume execution')
+    .option('--monitor', 'log output')
     .action(async (port, options) => {
       const portNr = Number(port);
-      if (isNaN(portNr)) {
-        program.error(`not a valid port nr`);
+      if (isNaN(portNr) && !isSerialPort(port)) {
+        program.error(`not a valid (Serial) port`);
+      }
+
+      const baudRate = Number(options.b);
+      if (isNaN(portNr) && isNaN(baudRate)) {
+        program.error(
+          `baudrate number is required when port is a serial port. Use -b to provide baudrate`,
+        );
+        return;
       }
 
       if (options.pause === undefined && options.resume === undefined) {
@@ -22,21 +36,31 @@ export function registerRunningRequestCommand(program: Command): void {
         return;
       }
 
-      const socket = new ClientSideSocket(portNr, 'localhost');
+      let channel: SerialConnection | ClientSideSocket | undefined;
+      if (!isNaN(portNr)) {
+        channel = new ClientSideSocket(portNr, 'localhost');
+      } else {
+        channel = new SerialConnection(port, baudRate);
+      }
+      if (options.monitor) {
+        channel.addOnData(console.log);
+      }
       const timeout = 100000;
-      if (!(await socket.open(timeout))) {
-        program.error(`failed to open connection to localhost:${portNr}`);
+      if (!(await channel.open(timeout))) {
+        program.error(`failed to open connection to ${channel.channelName}`);
         return;
       }
 
       try {
         const req =
           options.pause !== undefined ? new PauseRequest() : new RunRequest();
-        await sendRequest(socket, req, timeout);
+        await sendRequest(channel, req, timeout);
       } catch (e) {
         const errMsg = e instanceof Error ? `${e.message}${e.stack ?? ''}` : e;
         program.error(`Failed during ${commandName} error occurred: ${errMsg}`);
       }
-      socket.close().catch(console.error);
+      if (options.monitor === undefined) {
+        channel.close().catch(console.error);
+      }
     });
 }
