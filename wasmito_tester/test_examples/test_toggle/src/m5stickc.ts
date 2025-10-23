bd4bc2996b616920e45f511130253c18994413c5
carllocos <23368410+carllocos@users.noreply.github.com>
2025-10-23 16:34:06 +0200

    update test example

---
+++ wasmito_tester/test_examples/test_toggle/src/m5stickc.ts
@@ -0,0 +1,3 @@
+export const LED: u8 = 10;
+export const BTN_A: u8 = 37;
+export const BTN_B: u8 = 39;
---
+++ wasmito_tester/test_examples/test_toggle/test/test_led_toggle.ts
@@ -0,0 +1,82 @@
+import {
+  createSystemSetup,
+  oneM5StickCDev,
+  oneM5StickCMCU,
+} from '../../../reausable_system_setups';
+import { SystemTester, type TestScenario } from '../../../system_tester';
+import {
+  type TestScenarioResult,
+  type TestProgram,
+  type PostSetupConfig,
+} from '../../../shared_interfaces';
+import { TargetLanguage } from '../../../../src/compilers/prog_language_selection';
+import {
+  addBreakpointSubscription,
+  PauseAction,
+  runVMAction,
+  SubscribeOnBPReached,
+  TriggerInterrupt,
+} from '../../../reusable_actions';
+import { Breakpoint } from '../../../../src/debugger/breakpoint';
+import { type WasmCompilerArgs } from '../../../../src/compilers/wasm_compiler';
+
+/**
+ * Device Config
+ */
+const postSetupConfigM5Dev: PostSetupConfig = {
+  pauseAfterSetup: true,
+  actions: [],
+};
+const m5stickDev = oneM5StickCDev('m5StickCDev', postSetupConfigM5Dev);
+m5stickDev.disableStrictModuleLoad = true;
+
+const mcu = oneM5StickCMCU('m5stickCMCU', postSetupConfigM5Dev);
+mcu.serialPort = '/dev/cu.usbserial-8952FFEE8B';
+mcu.fqbn = 'm5stack:esp32:m5stick-c';
+
+const systemSetup = createSystemSetup('DevVM', [m5stickDev, mcu]);
+const arg: WasmCompilerArgs = {
+  wasmPath: './wasmito_tester/test_examples/test_toggle/wasm/toggle.wasm',
+  mappingsJSON: './wasmito_tester/test_examples/test_toggle/wasm/mappings.json',
+};
+
+const program: TestProgram = {
+  targetLanguage: TargetLanguage.Wasm,
+  sourceCodeCompilationArgs: arg,
+};
+
+const ButtonPin = 37;
+const subscriptionID = 'break on linenr 41 col 5';
+const testLoadAndRunModule: TestScenario = {
+  testName: 'Test If `ToggleLed` updates `LED_STATE`',
+  testProgram: program,
+  actions: [
+    addBreakpointSubscription(
+      subscriptionID,
+      new Breakpoint({
+        linenr: 41,
+        colnr: 5,
+        address: 0,
+        source: '',
+        name: '',
+      }),
+    ),
+    runVMAction(),
+    PauseAction(undefined, 3000),
+    TriggerInterrupt(ButtonPin, 3000),
+  ],
+  expect: [
+    runVMAction(undefined, 5000),
+    SubscribeOnBPReached(subscriptionID, 3000),
+    runVMAction(undefined, 3000),
+    TriggerInterrupt(ButtonPin, undefined, 5000),
+    SubscribeOnBPReached(subscriptionID, 3000),
+  ],
+};
+
+export async function run(): Promise<TestScenarioResult[]> {
+  const tester = new SystemTester(systemSetup);
+  tester.addTestScenario(testLoadAndRunModule, m5stickDev.id);
+  tester.addTestScenario(testLoadAndRunModule, mcu.id);
+  return await tester.runTests();
+}
