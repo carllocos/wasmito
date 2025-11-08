f71aa13f06a8943c05a6fce07863035f557f723c
carllocos <23368410+carllocos@users.noreply.github.com>
2025-11-08 23:22:06 +0100

    CLI wasm: add command to remove custom section

--- cli/wasm_command.ts
+++ cli/wasm_command.ts
@@ -2,7 +2,8 @@ import { type Command } from 'commander';
 import { isFilePath } from '../src/util/file_util';
 import { getGlobalLogger } from '../src/logger/logger';
 import { WasmModule } from '../src/webassembly/wasm/wasm_module';
-import { writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import { StripConfig, StripError } from 'wasmito-tools';
 
 export function registerWasmModuleCommand(program: Command): void {
   program
@@ -12,6 +13,10 @@ export function registerWasmModuleCommand(program: Command): void {
     .option(
       '-s,--store-instructions <output-path.json>',
       `store the instructions of the wasm module in a JSON file`,
+    )
+    .option(
+      '--strip-custom <path-to-stripped.wasm>',
+      `remove the custom section and store to given path`,
     )
     .action(async (wasmPath, options) => {
       const logger = getGlobalLogger();
@@ -25,11 +30,24 @@ export function registerWasmModuleCommand(program: Command): void {
         if (storePath !== undefined) {
           const obj = m.toJSON();
           const content = JSON.stringify(obj);
+          logger.info(`storing Wasm instructions to '${storePath}'`);
           writeFileSync(storePath, content);
         }
+
+        const stripPath = options.stripCustom;
+        if (stripPath !== undefined) {
+          const stripConfig = new StripConfig(true, []);
+          const buffer = readFileSync(wasmPath);
+          const stripped = stripConfig.strip(buffer);
+          logger.info(`storing stripped Wasm to '${stripPath}'`);
+          writeFileSync(stripPath, stripped);
+        }
       } catch (e) {
-        const errMsg = e instanceof Error ? `${e.message}${e.stack ?? ''}` : e;
-        program.error(`Could not be parsed error occurred: ${errMsg}`);
+        let errMsg = '';
+        if (e instanceof StripError) errMsg += e.context;
+        else if (e instanceof Error) errMsg += `${e.message}${e.stack ?? ''}`;
+        else errMsg += `${e}`;
+        program.error(`wasm command failed due : ${errMsg}`);
       }
     });
 }
