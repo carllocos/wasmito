3ba1cc01f7fcbdf8a721bb957064f50732185d42
carllocos <23368410+carllocos@users.noreply.github.com>
2025-11-13 18:09:24 +0100

    refactor subscribe listeners into source file

---
+++ src/hooks/isubscribe.ts
@@ -0,0 +1,95 @@
+import { Logger } from '../logger/logger';
+
+export interface ISubscription<SubscriptionType> {
+  readonly subscribe: (
+    calllback: (value: SubscriptionType) => void,
+    oneTimeSubscription: boolean,
+  ) => void;
+  readonly unSubscribe: (calllback: (value: SubscriptionType) => void) => void;
+  readonly onSubscriptionData: (data: SubscriptionType) => void;
+  readonly parseSubscriptionData: (input: any) => SubscriptionType;
+  readonly clearSubscriptions: () => void;
+}
+
+export abstract class ASubscription<SubscriptionType>
+  implements ISubscription<SubscriptionType>
+{
+  private listeners: Array<(data: SubscriptionType) => void>;
+  private oneTimeListeners: Array<(data: SubscriptionType) => void>;
+  private readonly removedListeners: Set<(data: SubscriptionType) => void>;
+  protected logger: Logger;
+
+  constructor(logger: Logger) {
+    this.listeners = [];
+    this.oneTimeListeners = [];
+    this.removedListeners = new Set();
+    this.logger = logger;
+  }
+
+  public subscribe(
+    callback: (data: SubscriptionType) => void,
+    oneTimeSubscription: boolean,
+  ): void {
+    let lstnrs: Array<(data: SubscriptionType) => void> = [];
+    if (oneTimeSubscription) {
+      lstnrs = this.oneTimeListeners;
+    } else {
+      lstnrs = this.listeners;
+    }
+    const found = lstnrs.find((cb) => cb === callback);
+    if (found !== undefined) {
+      this.logger.warn(`Attempting to add 2 same subscription callbacks`);
+      return;
+    }
+
+    lstnrs.push(callback);
+  }
+
+  public unSubscribe(callback: (data: SubscriptionType) => void): void {
+    this.removedListeners.add(callback);
+  }
+
+  onSubscriptionData(value: SubscriptionType): void {
+    if (this.listeners.length === 0 && this.oneTimeListeners.length === 0) {
+      return;
+    }
+    this.listeners.forEach((listener) => {
+      if (!this.removedListeners.has(listener)) {
+        listener(value);
+      }
+    });
+    this.listeners = this.listeners.filter((cb) => {
+      return !this.removedListeners.has(cb);
+    });
+    this.removedListeners.clear();
+    this.oneTimeListeners.forEach((listener) => {
+      listener(value);
+    });
+    this.oneTimeListeners = [];
+  }
+
+  clearSubscriptions(): void {
+    this.removedListeners.clear();
+    this.oneTimeListeners.length = 0;
+    this.listeners.length = 0;
+  }
+  abstract parseSubscriptionData(input: any): SubscriptionType;
+}
+
+export class Subscription<
+  SubscriptionType,
+> extends ASubscription<SubscriptionType> {
+  private parse: (input: any) => SubscriptionType;
+
+  constructor(
+    parseSubscriptionData: (input: any) => SubscriptionType,
+    logger: Logger,
+  ) {
+    super(logger);
+    this.parse = parseSubscriptionData;
+  }
+
+  override parseSubscriptionData(input: any): SubscriptionType {
+    return this.parse(input);
+  }
+}
