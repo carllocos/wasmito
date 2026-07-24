ddee001bbc3d0262a510e2c290b1ec9679c93ae0
carllocos <23368410+carllocos@users.noreply.github.com>
2026-07-24 12:20:10 +0200

    change requests handling
    - remove SendRequest to APIRequest interace to reduce memory use
    - changed APIRequest to keep track of correctly parsed VM content this reduces the need to create new arrays of response. Instead the APIRequest array can be reused.
    - Changed the typesignature of sendRequests to no longer return an array of responses as the requests now contain the responses
    - use a global promise to resolve when all bulk requests got answered instead of individual promises

--- src/runtimes/request_interface.ts
+++ src/runtimes/request_interface.ts
@@ -1,8 +1,14 @@
 import { type Channel } from '../communication/channel_interface';
 import { IDGenerator, RequestID } from '../communication/id_generator';
-import { RequestsManager } from '../communication/requests_manager';
+import {
+  CommandError,
+  RequestsManager,
+} from '../communication/requests_manager';
+import { createLogger } from '../logger/logger';
 import { RequestMessage } from './request_msg';
 import { type Instruction } from './wasmito_vm/requests/instructions';
+
+const logger = createLogger('RequestManager');
 
 export enum SubscriptionParseOutcome {
   Successful,
@@ -15,19 +21,126 @@ const idGenerator = new IDGenerator();
 export abstract class APIRequest<R> {
   public readonly id: RequestID;
   abstract readonly instruction: Instruction;
+  private _response: RequestMessage | undefined;
+  private _parsed: R | undefined;
+
+  private _resolved: boolean;
+  private _rejected: boolean;
+  public promise: any;
+  private resolver: ((value: R | PromiseLike<R>) => void) | undefined;
+  private rejector: ((reason?: any) => void) | undefined;
+
+  private cb?: () => void;
+
   constructor() {
     this.id = idGenerator.newID();
+    this.promise = new Promise((resolve, reject) => {
+      this.resolver = resolve;
+      this.rejector = reject;
+    });
+
+    this._resolved = false;
+    this._rejected = false;
   }
+
   abstract description(): string;
   abstract getData(): string;
   abstract parse(input: string): R; // TODO remove
   abstract processAck(ack: RequestMessage): R;
-  abstract processSubscriptionData(
+  async processSubscriptionData(
     sub: RequestMessage,
-  ): Promise<SubscriptionParseOutcome>;
+  ): Promise<SubscriptionParseOutcome> {
+    if (this.isSubscriptionClosed()) return SubscriptionParseOutcome.Failed;
+    return await this.processSubscriptionData(sub);
+  }
   abstract isSubscriptionClosed(): boolean;
+
   serializeID(): string {
     return idGenerator.serialiseIDToLEBHex(this.id);
+  }
+
+  get responseMessage(): RequestMessage {
+    if (this._response === undefined) {
+      throw new Error(`Request has not been send`);
+    }
+    return this._response;
+  }
+
+  get responseContent(): R {
+    if (this._parsed === undefined) {
+      throw new Error(`request has no response content`);
+    }
+    return this._parsed;
+  }
+
+  hasResponse(): boolean {
+    return this._response !== undefined;
+  }
+
+  registerTimeout(timeoutMs?: number) {
+    // TODO use timeout
+    if (timeoutMs !== undefined) {
+      setTimeout(() => {
+        this.timedout(timeoutMs);
+      }, timeoutMs);
+    }
+  }
+
+  isResolved(): boolean {
+    return this._rejected || this._resolved;
+  }
+
+  timedout(timeoutMs: number): void {
+    if (!this._rejected && !this._resolved) {
+      const errMsg = `Request ${this.description()} timedout after ${
+        timeoutMs
+      } ms while waiting for reply`;
+      logger.error(errMsg);
+      this.requestRejector(new CommandError(errMsg));
+    }
+  }
+
+  async processRequestMessage(
+    msg: RequestMessage,
+  ): Promise<SubscriptionParseOutcome> {
+    if (this._rejected) return SubscriptionParseOutcome.Failed;
+
+    if (this._resolved) {
+      // case we may feed data to a subscription
+      // if(this.isSubscriptionClosed()){}
+      return await this.processSubscriptionData(msg);
+    } else {
+      return this.processRequestAck(msg);
+    }
+  }
+
+  private processRequestAck(msg: RequestMessage): SubscriptionParseOutcome {
+    try {
+      const parsed = this.processAck(msg);
+      this._response = msg;
+      this._parsed = parsed;
+      this.requestResolver(parsed);
+      return SubscriptionParseOutcome.Successful;
+    } catch (_err) {
+      return SubscriptionParseOutcome.Failed;
+    }
+  }
+
+  private requestResolver(v: R): void {
+    if (!this._resolved && !this._rejected) {
+      this._resolved = true;
+      if (this.cb !== undefined) {
+        this.cb();
+      }
+      this.resolver!(v);
+    }
+  }
+
+  private requestRejector(v?: any): void {
+    if (!this._resolved && !this._rejected) {
+      this._rejected = true;
+      this.rejector!(v);
+    }
   }
 }
 
