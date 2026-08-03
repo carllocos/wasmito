b3fb23c59908797a8382bc0ded8ef86af76774ef
carllocos <23368410+carllocos@users.noreply.github.com>
2026-08-03 16:15:34 +0200

    add misisng generic type

--- test/shared/mock_channel.ts
+++ test/shared/mock_channel.ts
@@ -7,7 +7,7 @@ export class MockChannel implements Channel {
   readonly channelName: string = 'MockChannel';
   private dataHandler?: (data: string) => void;
   private mockWriteHandler?: (data: any) => boolean;
-  private writeListeners: Subscription<string | Uint8Array>;
+  private writeListeners: Subscription<string, string | Uint8Array>;
 
   constructor() {
     this.writeListeners = new Subscription(
