a9720f6cb8029ec530b7a64257377eb58052053e
carllocos <23368410+carllocos@users.noreply.github.com>
2026-08-04 17:38:22 +0200

    add tests for before & after instructions

---
+++ test/test_advices/test_after_multiple.test.ts
@@ -0,0 +1,196 @@
+import { expect } from 'chai';
+import path from 'path';
+import { WasmitoBackendVM } from '../../src/runtimes/wasmito_vm/wasmito_vm';
+import { WasmAnalysis } from '../../src/tool_api/wasm_analysis';
+import { WasmModule } from '../../src/webassembly/wasm/wasm_module';
+import { spawnDevVM } from '../../tool_examples/spawn_vm';
+import {
+  ReadOnlyWasmValue,
+  WritableWasmValue,
+} from '../../src/tool_api/interrupts';
+import { WasmInstruction } from '../../src/webassembly/wasm/wasm_instruction';
+
+describe('Test Multiple after advices on same Instruction', function () {
+  let wasm: WasmModule;
+  let vmConnection: WasmitoBackendVM;
+  let analysis: WasmAnalysis;
+  this.timeout(0);
+
+  before('Parse Module', async () => {
+    const wasmPath = path.resolve('./test/data/wat/fac/fac.wasm');
+    wasm = new WasmModule(wasmPath);
+  });
+
+  beforeEach(async () => {
+    vmConnection = await spawnDevVM(wasm); // for local VM
+    // vmConnection = await connectToExistingDevVM(wasm, 8192); // for local VM
+    analysis = new WasmAnalysis(wasm, vmConnection);
+  });
+
+  it('registering multiple advices after fac(5) should execute each once', async () => {
+    const mainFunc = wasm.getMainFunction();
+    const callFac = mainFunc.allInstructions[1];
+    let adviceOneCalled = 0;
+    let adviceTwoCalled = 0;
+    let adviceThreeCalled = 0;
+    analysis.after(callFac, () => adviceOneCalled++);
+    analysis.after(callFac, () => adviceTwoCalled++);
+    analysis.after(callFac, () => adviceThreeCalled++);
+
+    await analysis.deploy();
+    await analysis.run();
+    expect(adviceOneCalled).equal(1);
+    expect(adviceTwoCalled).equal(1);
+    expect(adviceThreeCalled).equal(1);
+  });
+
+  it('change before fac(5) to fac(1) then to fac(3)', async () => {
+    const mainFunc = wasm.getMainFunction();
+    const i32ConstFacArg = mainFunc.allInstructions[0];
+    const callFac = mainFunc.allInstructions[1];
+    let val1: number | undefined;
+    let val2: number | undefined;
+    let val3: number | undefined;
+    let val4: number | undefined;
+
+    analysis.afterMut(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: WritableWasmValue | undefined) => {
+        val1 = result?.value;
+        if (result !== undefined) result.value = 1;
+        return result;
+      },
+    );
+
+    analysis.afterMut(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: WritableWasmValue | undefined) => {
+        val2 = result?.value;
+        if (result !== undefined) result.value = 3;
+        return result;
+      },
+    );
+
+    analysis.after(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val3 = result?.value;
+      },
+    );
+
+    analysis.after(
+      callFac,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val4 = result?.value;
+      },
+    );
+
+    await analysis.deploy();
+    await analysis.run();
+
+    expect(val1).equal(5);
+    expect(val2).equal(1);
+    expect(val3).equal(3);
+    expect(val4).equal(6);
+  });
+
+  it('read after i32const 5, then change it to 32const 1, then read argument i32const', async () => {
+    const mainFunc = wasm.getMainFunction();
+    const i32ConstFacArg = mainFunc.allInstructions[0];
+    const callFac = mainFunc.allInstructions[1];
+    let val1: number | undefined;
+    let val2: number | undefined;
+    let val3: number | undefined;
+    let val4: number | undefined;
+
+    analysis.after(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val1 = result?.value;
+        return result;
+      },
+    );
+
+    analysis.afterMut(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: WritableWasmValue | undefined) => {
+        val2 = result?.value;
+        if (result !== undefined) result.value = 3;
+        return result;
+      },
+    );
+
+    analysis.after(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val3 = result?.value;
+        return result;
+      },
+    );
+
+    analysis.after(
+      callFac,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val4 = result?.value;
+      },
+    );
+
+    await analysis.deploy();
+    await analysis.run();
+
+    expect(val1).equal(5);
+    expect(val2).equal(5);
+    expect(val3).equal(3);
+    expect(val4).equal(6);
+  });
+
+  it('write i32.const 5 to i32.const 2, then read i32.const twice in a row', async () => {
+    const mainFunc = wasm.getMainFunction();
+    const i32ConstFacArg = mainFunc.allInstructions[0];
+    const callFac = mainFunc.allInstructions[1];
+    let val1: number | undefined;
+    let val2: number | undefined;
+    let val3: number | undefined;
+    let val4: number | undefined;
+
+    analysis.afterMut(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: WritableWasmValue | undefined) => {
+        val1 = result?.value;
+        if (result !== undefined) result.value = 2;
+        return result;
+      },
+    );
+
+    analysis.after(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val2 = result?.value;
+        return result;
+      },
+    );
+
+    analysis.after(
+      i32ConstFacArg,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val3 = result?.value;
+        return result;
+      },
+    );
+
+    analysis.after(
+      callFac,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val4 = result?.value;
+      },
+    );
+
+    await analysis.deploy();
+    await analysis.run();
+
+    expect(val1).equal(5);
+    expect(val2).equal(2);
+    expect(val3).equal(2);
+    expect(val4).equal(2);
+  });
+});
---
+++ test/test_advices/test_before_mutability.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import path from 'path';
+import { WasmModule } from '../../src/webassembly/wasm/wasm_module';
+import { spawnDevVM } from '../../tool_examples/spawn_vm';
+import { WasmAnalysis } from '../../src/tool_api/wasm_analysis';
+import {
+  ReadOnlyWasmValue,
+  WritableWasmValue,
+} from '../../src/tool_api/interrupts';
+import { WasmInstruction } from '../../src/webassembly/wasm/wasm_instruction';
+import { WasmitoBackendVM } from '../../src/runtimes/wasmito_vm/wasmito_vm';
+
+describe('Test before Mutability', function () {
+  let wasm: WasmModule;
+  let vmConnection: WasmitoBackendVM;
+  let analysis: WasmAnalysis;
+  this.timeout(0);
+
+  before('Parse Module', async () => {
+    const wasmPath = path.resolve('./test/data/wat/fac/fac.wasm');
+    wasm = new WasmModule(wasmPath);
+  });
+
+  beforeEach(async () => {
+    vmConnection = await spawnDevVM(wasm); // for local VM
+    // vmConnection = await connectToExistingDevVM(wasm, 8192); // for local VM
+    analysis = new WasmAnalysis(wasm, vmConnection);
+  });
+
+  it('fac(5) to fac(1)', async () => {
+    const mainFunc = wasm.getMainFunction();
+    const callFac = mainFunc.allInstructions[1];
+
+    let val1: number | undefined;
+    let val2: number | undefined;
+    analysis.beforeMut(
+      callFac,
+      (_i: WasmInstruction, args: WritableWasmValue[]) => {
+        val1 = args[0].value;
+        args[0].value = 1;
+        return args;
+      },
+    );
+
+    analysis.after(
+      callFac,
+      (_i: WasmInstruction, result: ReadOnlyWasmValue | undefined) => {
+        val2 = result?.value;
+      },
+    );
+
+    await analysis.deploy();
+    await analysis.run();
+    expect(val1).equal(5);
+    expect(val2).equal(1);
+  });
+});
