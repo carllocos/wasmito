900879c84763b033092c4a5211f9a799df6c914d
carllocos <23368410+carllocos@users.noreply.github.com>
2026-08-28 15:00:23 +0200

    add: subclasses for readonly and write const

--- src/tool_api/interrupts.ts
+++ src/tool_api/interrupts.ts
@@ -61,9 +61,9 @@ export class ReadOnlyInterrupt {
 }
 
 export class ReadOnlyWasmValue {
-  private _value: number | bigint;
-  private _type: WASM.Type;
-  private _stackIdx: number;
+  protected _value: number | bigint;
+  protected _type: WASM.Type;
+  protected _stackIdx: number;
 
   constructor(type: WASM.Type, value: number | bigint, stackIdx = -1) {
     this._value = value;
@@ -92,12 +92,90 @@ export class ReadOnlyWasmValue {
       type: this.type,
     };
   }
+
+  static new(
+    type: WASM.Type,
+    value: number | bigint,
+    stackIdx = -1,
+  ): ReadOnlyWasmValue {
+    switch (type) {
+      case WASM.Type.i64:
+        return new ReadOnlyI64Const(value, stackIdx);
+      default:
+        assert(
+          typeof value === 'number',
+          `value is expected to be a number. Value is currently ${typeof value}`,
+        );
+        switch (type) {
+          case WASM.Type.i32:
+            return new ReadOnlyI32Const(value, stackIdx);
+          case WASM.Type.f32:
+            return new ReadOnlyF32Const(value, stackIdx);
+          case WASM.Type.f64:
+            return new ReadOnlyF64Const(value, stackIdx);
+          default:
+            throw new Error(`cannot create Wasm const with type ${type}`);
+        }
+    }
+  }
+
+  static isI32Const(v: WritableWasmValue): v is WritableI32Const {
+    return v instanceof WritableI32Const;
+  }
+
+  static isI64Const(v: WritableWasmValue): v is WritableI64Const {
+    return v instanceof WritableI64Const;
+  }
+
+  static isF32Const(v: WritableWasmValue): v is WritableF32Const {
+    return v instanceof WritableF32Const;
+  }
+
+  static isF64Const(v: WritableWasmValue): v is WritableF64Const {
+    return v instanceof WritableF64Const;
+  }
+}
+
+export class ReadOnlyI32Const extends ReadOnlyWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.i32, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
+}
+
+export class ReadOnlyI64Const extends ReadOnlyWasmValue {
+  constructor(value: number | bigint, stackIdx = -1) {
+    super(WASM.Type.i64, value, stackIdx);
+  }
+}
+
+export class ReadOnlyF32Const extends ReadOnlyWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.f32, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
+}
+
+export class ReadOnlyF64Const extends ReadOnlyWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.f64, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
 }
 
 export class WritableWasmValue {
-  private _value: number | bigint;
-  private _type: WASM.Type;
-  private _stackIdx: number;
+  protected _value: number | bigint;
+  protected _type: WASM.Type;
+  protected _stackIdx: number;
   constructor(type: WASM.Type, value: number | bigint, stackIdx = -1) {
     this._value = value;
     this._type = type;
@@ -129,6 +207,96 @@ export class WritableWasmValue {
       value: this.value,
       type: this.type,
     };
+  }
+
+  static new(
+    type: WASM.Type,
+    value: number | bigint,
+    stackIdx = -1,
+  ): WritableWasmValue {
+    switch (type) {
+      case WASM.Type.i64:
+        return new WritableI64Const(value, stackIdx);
+      default:
+        assert(
+          typeof value === 'number',
+          `value is expected to be a number. Value is currently ${typeof value}`,
+        );
+        switch (type) {
+          case WASM.Type.i32:
+            return new WritableI32Const(value, stackIdx);
+          case WASM.Type.f32:
+            return new WritableF32Const(value, stackIdx);
+          case WASM.Type.f64:
+            return new WritableF64Const(value, stackIdx);
+          default:
+            throw new Error(`cannot create Wasm const with type ${type}`);
+        }
+    }
+  }
+
+  static isI32Const(v: WritableWasmValue): v is WritableI32Const {
+    return v instanceof WritableI32Const;
+  }
+
+  static isI64Const(v: WritableWasmValue): v is WritableI64Const {
+    return v instanceof WritableI64Const;
+  }
+
+  static isF32Const(v: WritableWasmValue): v is WritableF32Const {
+    return v instanceof WritableF32Const;
+  }
+
+  static isF64Const(v: WritableWasmValue): v is WritableF64Const {
+    return v instanceof WritableF64Const;
+  }
+}
+
+export class WritableI32Const extends WritableWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.i32, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
+
+  set value(n: number) {
+    this._value = n;
+  }
+}
+
+export class WritableI64Const extends WritableWasmValue {
+  constructor(value: number | bigint, stackIdx = -1) {
+    super(WASM.Type.i64, value, stackIdx);
+  }
+}
+
+export class WritableF32Const extends WritableWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.f32, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
+
+  set value(n: number) {
+    this._value = n;
+  }
+}
+
+export class WritableF64Const extends WritableWasmValue {
+  constructor(value: number, stackIdx = -1) {
+    super(WASM.Type.f64, value, stackIdx);
+  }
+
+  get value(): number {
+    return this._value as number;
+  }
+
+  set value(n: number) {
+    this._value = n;
   }
 }
 
